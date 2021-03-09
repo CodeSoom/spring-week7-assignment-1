8357f1329fecee36a7cfa46196ad7e1b132ca05b
newoo <newoo4297@naver.com>
2021-03-09 18:07:08 +0900

    jwt를 위한 인증 필터 객체생성하는 곳에 인증요청을 처리하는 객체 추가

--- app/src/main/java/com/codesoom/assignment/config/SecurityJavaConfig.java
+++ app/src/main/java/com/codesoom/assignment/config/SecurityJavaConfig.java
@@ -4,7 +4,6 @@ import com.codesoom.assignment.filters.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.AuthenticationFilter;
 
 import javax.servlet.Filter;
 
@@ -15,7 +14,9 @@ import javax.servlet.Filter;
 public class SecurityJavaConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        Filter authenticationFilter = new JwtAuthenticationFilter();
+        Filter authenticationFilter = new JwtAuthenticationFilter(
+                authenticationManager());
+
         http.csrf().disable()
                 .addFilter(authenticationFilter);
     }
--- app/src/main/java/com/codesoom/assignment/filters/JwtAuthenticationFilter.java
+++ app/src/main/java/com/codesoom/assignment/filters/JwtAuthenticationFilter.java
@@ -3,6 +3,9 @@ package com.codesoom.assignment.filters;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
+/**
+ * Jwt 인증을 위한 필터 클래스
+ */
 public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
     public JwtAuthenticationFilter(AuthenticationManager authenticationManager) {
         super(authenticationManager);
