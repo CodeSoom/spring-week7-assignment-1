5008a40f4550d2f2fd1f64132a0d438cbd85aa43
etff <wish2rich@gmail.com>
2021-03-09 11:57:34 +0900

    feature: add preAuthorized check

--- app/src/main/java/com/codesoom/assignment/config/SecurityConfig.java
+++ app/src/main/java/com/codesoom/assignment/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import com.codesoom.assignment.filters.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -14,6 +15,7 @@ import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import javax.servlet.Filter;
 
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final AuthenticationService authenticationService;
