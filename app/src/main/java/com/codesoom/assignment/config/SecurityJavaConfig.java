2cc7db1c0cefb8abac559d00b7d04db5c96de930
mikekang47 <gyeong0308@gmail.com>
2021-03-09 15:33:58 +0900

    Created SecurityJavaConfig class added http.csrf disable to proted from post attack

---
+++ app/src/main/java/com/codesoom/assignment/config/SecurityJavaConfig.java
@@ -0,0 +1,17 @@
+package com.codesoom.assignment.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityJavaConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable();
+    }
+
+
+}
