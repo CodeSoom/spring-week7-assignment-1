2441446d2830959d2938c675062696a1140a9247
wenodev <logicalstark1105@gmail.com>
2021-03-10 08:19:16 +0900

    feat: doFilterInternal를 사용하여 security chain에 끼어들도록 하라

--- app/src/main/java/com/codesoom/assignment/filters/JwtAuthenticationFilter.java
+++ app/src/main/java/com/codesoom/assignment/filters/JwtAuthenticationFilter.java
@@ -3,8 +3,21 @@ package com.codesoom.assignment.filters;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
     public JwtAuthenticationFilter(AuthenticationManager authenticationManager) {
         super(authenticationManager);
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain chain) throws IOException, ServletException {
+        super.doFilterInternal(request, response, chain);
     }
 }
