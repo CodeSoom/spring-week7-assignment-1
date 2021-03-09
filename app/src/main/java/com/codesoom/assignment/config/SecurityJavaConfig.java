bd1b04f828fe1a3664e754b0bfd880e0a6792574
developerOlive <chloehiho@gmail.com>
2021-03-09 18:32:52 +0900

    Feat : 필터를 사용하기 위해 authenticationFilter 생성
    - JwtAuthenticationFilter을 사용하기 위해서 authenticationManager()을 매개변수로 받음
    (authenticationManager : Spring Security의 필터들이 인증을 수행하는 방법에 대한 명세를 정의해 놓은 인터페이스)

--- app/src/main/java/com/codesoom/assignment/config/SecurityJavaConfig.java
+++ app/src/main/java/com/codesoom/assignment/config/SecurityJavaConfig.java
@@ -1,17 +1,26 @@
 package com.codesoom.assignment.config;
 
+import com.codesoom.assignment.filters.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.AuthenticationFilter;
+
+import javax.servlet.Filter;
 
 @Configuration
 public class SecurityJavaConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
+        Filter authenticationFilter = new JwtAuthenticationFilter(
+                authenticationManager());
+
         // CSRF(Cross-Site Request Forgery) 비활성화
         http
-                .csrf().disable();
+                .csrf().disable()
+                .addFilter(authenticationFilter);
 
     }
 }
