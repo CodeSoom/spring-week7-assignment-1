aa409b0b1b353677d66f94e1a326b4d7109bd0d4
developerOlive <chloehiho@gmail.com>
2021-03-09 18:48:33 +0900

    Feat :  doFilterInternal 메소드 내 인증을 위한 코드 추가
    JwtAuthenticationFilter - authenticationService을 사용하기 위해 필드에 선언 후 @Autowired 추가, 생성자에도 추가
    SecurityJavaConfig - authenticationService을 사용하기 위해 필드에 선언 후 @Autowired 추가, 생성자에도 추가

--- app/src/main/java/com/codesoom/assignment/config/SecurityJavaConfig.java
+++ app/src/main/java/com/codesoom/assignment/config/SecurityJavaConfig.java
@@ -1,21 +1,24 @@
 package com.codesoom.assignment.config;
 
+import com.codesoom.assignment.application.AuthenticationService;
 import com.codesoom.assignment.filters.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.AuthenticationFilter;
 
 import javax.servlet.Filter;
 
 @Configuration
 public class SecurityJavaConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private AuthenticationService authenticationService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
         Filter authenticationFilter = new JwtAuthenticationFilter(
-                authenticationManager());
+                authenticationManager(), authenticationService);
 
         // CSRF(Cross-Site Request Forgery) 비활성화
         http
