20184b1f723cf704285c3de91c742a3a46cd376d
wenodev <logicalstark1105@gmail.com>
2021-03-10 07:54:42 +0900

    test: 로그인 페이지 비활성화 테스트하라

---
+++ app/src/main/java/com/codesoom/assignment/config/SecurityJavaConfig.java
@@ -0,0 +1,12 @@
+package com.codesoom.assignment.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityJavaConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+    }
+}
