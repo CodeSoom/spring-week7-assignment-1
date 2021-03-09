52e29be52d14e3291c9c1dee64f9a2ccd611b67f
wenodev <logicalstark1105@gmail.com>
2021-03-10 08:29:10 +0900

    test: access token 없을경우 테스트를 통과하라

--- app/src/main/java/com/codesoom/assignment/filters/JwtAuthenticationFilter.java
+++ app/src/main/java/com/codesoom/assignment/filters/JwtAuthenticationFilter.java
@@ -1,5 +1,7 @@
 package com.codesoom.assignment.filters;
 
+import com.codesoom.assignment.application.AuthenticationService;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
@@ -10,14 +12,31 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
-    public JwtAuthenticationFilter(AuthenticationManager authenticationManager) {
+    private final AuthenticationService authenticationService;
+
+    public JwtAuthenticationFilter(AuthenticationManager authenticationManager, AuthenticationService authenticationService) {
         super(authenticationManager);
+        this.authenticationService = authenticationService;
     }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain chain) throws IOException, ServletException {
-        super.doFilterInternal(request, response, chain);
+
+        String authorization = request.getHeader("Authorization");
+
+        if (authorization == null) {
+            response.sendError(HttpStatus.UNAUTHORIZED.value());
+            return;
+        }
+
+        String accessToken = authorization.substring("Bearer ".length());
+        Long userId = authenticationService.parseToken(accessToken);
+
+        request.setAttribute("userId", userId);
+        
+        chain.doFilter(request, response);
+//        super.doFilterInternal(request, response, chain);
     }
 }
