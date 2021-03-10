b03b6730a14295867cf6bcd7bde083564ae539cc
양효주 <yhyojoo@icloud.com>
2021-03-10 21:01:34 +0900

    인터셉트의 인증 구현 부분을 JwtAuthenticationFilter로 이동
    인터셉트가 하던 역할을 SpringSecurity에서 처리할 수 있도록 인터셉트의
    인증 구현 부분을 JwtAuthenticationFilter로 이동

--- app/src/main/java/com/codesoom/assignment/filters/JwtAuthenticationFilter.java
+++ app/src/main/java/com/codesoom/assignment/filters/JwtAuthenticationFilter.java
@@ -1,11 +1,65 @@
 package com.codesoom.assignment.filters;
 
+import com.codesoom.assignment.application.AuthenticationService;
+import com.codesoom.assignment.errors.InvalidTokenException;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
+    private final AuthenticationService authenticationService;
     public JwtAuthenticationFilter(
-            AuthenticationManager authenticationManager) {
+            AuthenticationManager authenticationManager,
+            AuthenticationService authenticationService) {
         super(authenticationManager);
+        this.authenticationService = authenticationService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain chain)
+            throws IOException, ServletException {
+        if (filterWithPathAndMethod(request)) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        String authorization = request.getHeader("Authorization");
+
+        if (authorization == null) {
+            response.sendError(HttpStatus.UNAUTHORIZED.value());
+            return;
+        }
+
+        String accessToken = authorization.substring("Bearer ".length());
+
+        try {
+            authenticationService.parseToken(accessToken);
+        } catch (InvalidTokenException e) {
+            response.sendError(HttpStatus.UNAUTHORIZED.value());
+            return;
+        }
+
+        chain.doFilter(request, response);
+    }
+
+    private boolean filterWithPathAndMethod(HttpServletRequest request) {
+        String path = request.getRequestURI();
+        if (path.equals("/products")) {
+            return true;
+        }
+
+        String method = request.getMethod();
+        if (method.equals("GET")) {
+            return true;
+        }
+        return false;
     }
 }
