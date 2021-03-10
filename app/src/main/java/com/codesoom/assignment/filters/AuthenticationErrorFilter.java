ff7426c381f751d43d917d5d87b16351f3430910
양효주 <yhyojoo@icloud.com>
2021-03-10 21:37:00 +0900

    AuthenticationErrorFilter에 예외 처리 코드 작성

--- app/src/main/java/com/codesoom/assignment/filters/AuthenticationErrorFilter.java
+++ app/src/main/java/com/codesoom/assignment/filters/AuthenticationErrorFilter.java
@@ -1,5 +1,8 @@
 package com.codesoom.assignment.filters;
 
+import com.codesoom.assignment.errors.InvalidTokenException;
+import org.springframework.http.HttpStatus;
+
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpFilter;
@@ -9,7 +12,14 @@ import java.io.IOException;
 
 public class AuthenticationErrorFilter extends HttpFilter {
     @Override
-    protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
-        super.doFilter(request, response, chain);
+    protected void doFilter(HttpServletRequest request,
+                            HttpServletResponse response,
+                            FilterChain chain)
+            throws IOException, ServletException {
+        try {
+            chain.doFilter(request, response);
+        } catch (InvalidTokenException e) {
+            response.sendError(HttpStatus.UNAUTHORIZED.value());
+        }
     }
 }
