b346a9c0ec4bd28dddd2492b455e648a54dd7629
양효주 <yhyojoo@icloud.com>
2021-03-10 21:20:42 +0900

    JwtAuthenticationFilter에서 예외를 그대로 처리하도록 코드 수정

--- app/src/main/java/com/codesoom/assignment/filters/JwtAuthenticationFilter.java
+++ app/src/main/java/com/codesoom/assignment/filters/JwtAuthenticationFilter.java
@@ -2,7 +2,6 @@ package com.codesoom.assignment.filters;
 
 import com.codesoom.assignment.application.AuthenticationService;
 import com.codesoom.assignment.errors.InvalidTokenException;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
@@ -14,6 +13,7 @@ import java.io.IOException;
 
 public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
     private final AuthenticationService authenticationService;
+
     public JwtAuthenticationFilter(
             AuthenticationManager authenticationManager,
             AuthenticationService authenticationService) {
@@ -33,18 +33,13 @@ public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
 
         String authorization = request.getHeader("Authorization");
 
-        try {
-            if (authorization == null) {
-                throw new InvalidTokenException("");
-            }
-
-            String accessToken = authorization.substring("Bearer ".length());
-
-            authenticationService.parseToken(accessToken);
-        } catch (InvalidTokenException e) {
-            response.sendError(HttpStatus.UNAUTHORIZED.value());
-            return;
+        if (authorization == null) {
+            throw new InvalidTokenException("");
         }
+
+        String accessToken = authorization.substring("Bearer ".length());
+
+        authenticationService.parseToken(accessToken);
 
         chain.doFilter(request, response);
     }
