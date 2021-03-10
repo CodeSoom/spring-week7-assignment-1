1065531aa0d71708beae8129df6a7df38ed1a654
etff <wish2rich@gmail.com>
2021-03-10 17:30:19 +0900

    refactor: remove unused exception response

--- app/src/main/java/com/codesoom/assignment/controllers/ControllerErrorAdvice.java
+++ app/src/main/java/com/codesoom/assignment/controllers/ControllerErrorAdvice.java
@@ -2,7 +2,6 @@ package com.codesoom.assignment.controllers;
 
 import com.codesoom.assignment.dto.ErrorResponse;
 import com.codesoom.assignment.errors.AccessDeniedException;
-import com.codesoom.assignment.errors.InvalidTokenException;
 import com.codesoom.assignment.errors.LoginFailException;
 import com.codesoom.assignment.errors.ProductNotFoundException;
 import com.codesoom.assignment.errors.UserEmailDuplicationException;
@@ -12,10 +11,6 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-
-import javax.validation.ConstraintViolation;
-import javax.validation.ConstraintViolationException;
-import java.util.Set;
 
 @ResponseBody
 @ControllerAdvice
@@ -43,33 +38,7 @@ public class ControllerErrorAdvice {
     public ErrorResponse handleLoginFailException() {
         return new ErrorResponse("Log-in failed");
     }
-
-    @ResponseStatus(HttpStatus.UNAUTHORIZED)
-    @ExceptionHandler(InvalidTokenException.class)
-    public ErrorResponse handleInvalidAccessTokenException() {
-        return new ErrorResponse("Invalid access token");
-    }
-
-    @ResponseBody
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(ConstraintViolationException.class)
-    public ErrorResponse handleConstraintValidateError(
-            ConstraintViolationException exception
-    ) {
-        String messageTemplate = getViolatedMessage(exception);
-        return new ErrorResponse(messageTemplate);
-    }
-
-    private String getViolatedMessage(ConstraintViolationException exception) {
-        String messageTemplate = null;
-        Set<ConstraintViolation<?>> violations = exception.getConstraintViolations();
-        for (ConstraintViolation<?> violation : violations) {
-            messageTemplate = violation.getMessageTemplate();
-        }
-        return messageTemplate;
-    }
-
-    @ResponseBody
+    
     @ResponseStatus(HttpStatus.FORBIDDEN)
     @ExceptionHandler(AccessDeniedException.class)
     public ErrorResponse handleUserIdNotMatchException() {
