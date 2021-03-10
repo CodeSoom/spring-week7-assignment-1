134e09d1993a698281f085bfee28042fce3e25cb
양효주 <yhyojoo@icloud.com>
2021-03-10 20:29:06 +0900

    SecurityJavaConfig에 AuthenticationFilter 추가
    사용자 인증 처리를 위해 authenticationManager를 파라미터로 받는 AuthenticationFilter를 생성하고 SecurityJavaConfig에 추가

---
+++ app/src/main/java/com/codesoom/assignment/filters/JwtAuthenticationFilter.java
@@ -0,0 +1,11 @@
+package com.codesoom.assignment.filters;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
+    public JwtAuthenticationFilter(
+            AuthenticationManager authenticationManager) {
+        super(authenticationManager);
+    }
+}
