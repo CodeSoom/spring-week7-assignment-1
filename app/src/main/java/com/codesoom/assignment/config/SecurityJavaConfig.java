6eabda7028c60172a51ee9ff9a82cbfac4198701
thkim <thkim2017@gmail.com>
2021-03-11 16:00:56 +0900

    Create AuthenticationErrorFilter Apply it and delete useless files

--- app/src/main/java/com/codesoom/assignment/config/SecurityJavaConfig.java
+++ app/src/main/java/com/codesoom/assignment/config/SecurityJavaConfig.java
@@ -1,6 +1,7 @@
 package com.codesoom.assignment.config;
 
 import com.codesoom.assignment.application.AuthenticationService;
+import com.codesoom.assignment.filters.AuthenticationErrorFilter;
 import com.codesoom.assignment.filters.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -19,8 +20,12 @@ public class SecurityJavaConfig extends WebSecurityConfigurerAdapter {
         Filter authenticationFilter = new JwtAuthenticationFilter(
                 authenticationManager(), authenticationService);
 
+        Filter authenticationErrorFilter = new AuthenticationErrorFilter();
+
         http
                 .csrf().disable()
-                .addFilter(authenticationFilter);
+                .addFilter(authenticationFilter)
+                .addFilterBefore(authenticationErrorFilter,
+                        JwtAuthenticationFilter.class);
     }
 }
