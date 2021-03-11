3b3c3246e99b303df8e60aa4e5e7937f58463c5d
developerOlive <chloehiho@gmail.com>
2021-03-12 01:13:33 +0900

    Feat :  h2-console을 사용할 수 없는 에러 해결을 위한 코드 추가

--- app/src/main/java/com/codesoom/assignment/config/SecurityJavaConfig.java
+++ app/src/main/java/com/codesoom/assignment/config/SecurityJavaConfig.java
@@ -40,5 +40,6 @@ public class SecurityJavaConfig extends WebSecurityConfigurerAdapter {
                 .authenticationEntryPoint(
                         new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
 
+      http.headers().frameOptions().disable();
     }
 }
