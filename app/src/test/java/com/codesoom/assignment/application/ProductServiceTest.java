99008b04308a7169eff47ba1d90825a72d4aeffd
sedin <sedin@kakao.com>
2021-06-15 01:23:31 +0900

    Test: 테스트 수정
    - Java8 버전을 사용중이여서 메소드 수정

--- app/src/test/java/com/codesoom/assignment/application/ProductServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/application/ProductServiceTest.java
@@ -6,15 +6,16 @@
 
 package com.codesoom.assignment.application;
 
-import com.codesoom.assignment.errors.ProductNotFoundException;
 import com.codesoom.assignment.domain.Product;
 import com.codesoom.assignment.domain.ProductRepository;
 import com.codesoom.assignment.dto.ProductData;
+import com.codesoom.assignment.errors.ProductNotFoundException;
 import com.github.dozermapper.core.DozerBeanMapperBuilder;
 import com.github.dozermapper.core.Mapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -44,7 +45,7 @@ class ProductServiceTest {
                 .price(5000)
                 .build();
 
-        given(productRepository.findAll()).willReturn(List.of(product));
+        given(productRepository.findAll()).willReturn(Arrays.asList(product));
 
         given(productRepository.findById(1L)).willReturn(Optional.of(product));
 
@@ -61,7 +62,7 @@ class ProductServiceTest {
 
     @Test
     void getProductsWithNoProduct() {
-        given(productRepository.findAll()).willReturn(List.of());
+        given(productRepository.findAll()).willReturn(Arrays.asList());
 
         assertThat(productService.getProducts()).isEmpty();
     }
