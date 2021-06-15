dddafde7b1958bac57134f7eef07f36727d48b35
Lim <40200760+tbnsok40@users.noreply.github.com>
2021-06-15 11:53:12 +0900

    UserAuthentication클래스 생성

--- app/src/main/java/com/codesoom/assignment/security/UserAuthentication.java
+++ app/src/main/java/com/codesoom/assignment/security/UserAuthentication.java
@@ -1,2 +1,43 @@
-package com.codesoom.assignment.security;public class UserAuthentication {
+package com.codesoom.assignment.security;
+
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserAuthentication extends AbstractAuthenticationToken {
+    private final Long userId;
+
+    public UserAuthentication(Long userId) {
+        super(authorities());
+        this.userId = userId;
+    }
+
+    private static List<GrantedAuthority> authorities(){
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("USER"));
+        return authorities;
+    }
+
+    @Override
+    public Object getCredentials() {
+        return null;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return userId;
+    }
+
+    @Override
+    public boolean isAuthenticated() {
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "UserAuthentication{ " + userId + " }";
+    }
 }
