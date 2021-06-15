266a43cffe30442acdb5c1e76bb10f0a696cbbe6
Joowan Kim <rlawndhks217@gmail.com>
2021-06-15 14:36:29 +0900

    Add JavaDoc for AuthenticationService

--- app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
+++ app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
@@ -8,6 +8,9 @@ import io.jsonwebtoken.Claims;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+/**
+ * 인증 토큰을 관리합니다.
+ */
 @Service
 public class AuthenticationService {
     private final UserRepository userRepository;
@@ -22,6 +25,13 @@ public class AuthenticationService {
         this.jwtUtil = jwtUtil;
     }
 
+    /**
+     * 유저 정보를 검증하고 인증 토큰을 반환합니다.
+     *
+     * @param email 유저가 입력한 이메일
+     * @param password 유저가 입력한 비밀번호
+     * @return 인증 토큰
+     */
     public String login(String email, String password) {
         User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new LoginFailException(email));
@@ -30,9 +40,15 @@ public class AuthenticationService {
             throw new LoginFailException(email);
         }
 
-        return jwtUtil.encode(1L);
+        return jwtUtil.encode(user.getId());
     }
 
+    /**
+     * 전달 받은 인증 토큰을 해독하고 유저 식별자를 반환합니다.
+     *
+     * @param accessToken 인증 토큰
+     * @return 유저 식별자
+     */
     public Long parseToken(String accessToken) {
         Claims claims = jwtUtil.decode(accessToken);
         return claims.get("userId", Long.class);
