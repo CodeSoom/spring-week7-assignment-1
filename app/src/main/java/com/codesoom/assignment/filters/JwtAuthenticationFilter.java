2733a3a19bd19cb3166bce0d22f75d8ae181ed8b
youdongkwan <dkyou7@naver.com>
2021-06-19 17:23:10 +0900

    [Feature] JwtAuthenticationFilter.java 에 doFilterInternal method를 생성하였습니다.

--- app/src/main/java/com/codesoom/assignment/filters/JwtAuthenticationFilter.java
+++ app/src/main/java/com/codesoom/assignment/filters/JwtAuthenticationFilter.java
@@ -3,10 +3,25 @@ package com.codesoom.assignment.filters;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
 
     // authenticationManager : Spring Security의 필터들이 인증을 수행하는 방법에 대한 명세를 정의해 놓은 인터페이스
     public JwtAuthenticationFilter(AuthenticationManager authenticationManager) {
         super(authenticationManager);
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain chain)
+            throws IOException, ServletException {
+        // TODO : authentication
+        chain.doFilter(request, response);
     }
 }
