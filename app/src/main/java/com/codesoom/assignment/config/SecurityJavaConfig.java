04c48dfa5059c328ab508638cea35c1a41fc2590
youdongkwan <dkyou7@naver.com>
2021-06-19 19:28:21 +0900

    [Feature] doFilterInternal 메소드 내 인증을 위한 코드 추가하기 JwtAuthenticationFilter - authenticationService을 사용하기 위해 필드에 선언 후 @Autowired 추가, 생성자에도 추가 SecurityJavaConfig - authenticationService을 사용하기 위해 필드에 선언 후 @Autowired 추가, 생성자에도 추가

--- app/src/main/java/com/codesoom/assignment/config/SecurityJavaConfig.java
+++ app/src/main/java/com/codesoom/assignment/config/SecurityJavaConfig.java
@@ -1,6 +1,8 @@
 package com.codesoom.assignment.config;
 
+import com.codesoom.assignment.application.AuthenticationService;
 import com.codesoom.assignment.filters.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -15,6 +17,9 @@ import javax.servlet.Filter;
 @Configuration
 public class SecurityJavaConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private AuthenticationService authenticationService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
@@ -22,7 +27,7 @@ public class SecurityJavaConfig extends WebSecurityConfigurerAdapter {
          * JwtAuthenticationFilter을 사용하기 위해서 authenticationManager()을 매개변수로 받음
          * (authenticationManager : Spring Security의 필터들이 인증을 수행하는 방법에 대한 명세를 정의해 놓은 인터페이스)
          */
-        Filter authenticationFilter = new JwtAuthenticationFilter(authenticationManager());
+        Filter authenticationFilter = new JwtAuthenticationFilter(authenticationManager(), authenticationService);
 
         http
                 .csrf().disable()  // CSRF(Cross-Site Request Forgery) 비활성화
--- app/src/main/java/com/codesoom/assignment/filters/JwtAuthenticationFilter.java
+++ app/src/main/java/com/codesoom/assignment/filters/JwtAuthenticationFilter.java
@@ -1,5 +1,8 @@
 package com.codesoom.assignment.filters;
 
+import com.codesoom.assignment.application.AuthenticationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
@@ -11,9 +14,14 @@ import java.io.IOException;
 
 public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
 
+    @Autowired
+    private final AuthenticationService authenticationService;
+
     // authenticationManager : Spring Security의 필터들이 인증을 수행하는 방법에 대한 명세를 정의해 놓은 인터페이스
-    public JwtAuthenticationFilter(AuthenticationManager authenticationManager) {
+    public JwtAuthenticationFilter(AuthenticationManager authenticationManager,
+                                   AuthenticationService authenticationService) {
         super(authenticationManager);
+        this.authenticationService = authenticationService;
     }
 
     @Override
@@ -21,7 +29,17 @@ public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
                                     HttpServletResponse response,
                                     FilterChain chain)
             throws IOException, ServletException {
+
         // TODO : authentication
+        String authorization = request.getHeader("Authorization");
+
+        if (authorization == null) {
+            response.sendError(HttpStatus.UNAUTHORIZED.value());
+            return;
+        }
+
+        String accessToken = authorization.substring("Bearer ".length());
+        authenticationService.parseToken(accessToken);
         chain.doFilter(request, response);
     }
 }
