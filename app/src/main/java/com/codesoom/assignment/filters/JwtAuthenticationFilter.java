292819a9a5cf490bceeb54098ba3482f51233392
youdongkwan <dkyou7@naver.com>
2021-06-19 20:35:29 +0900

    [Refactoring] JwtAuthenticationFilter.java 권한 획득하는 부분 추가

--- app/src/main/java/com/codesoom/assignment/filters/JwtAuthenticationFilter.java
+++ app/src/main/java/com/codesoom/assignment/filters/JwtAuthenticationFilter.java
@@ -2,8 +2,12 @@ package com.codesoom.assignment.filters;
 
 import com.codesoom.assignment.application.AuthenticationService;
 import com.codesoom.assignment.errors.InvalidTokenException;
+import com.codesoom.assignment.security.UserAuthentication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 import javax.servlet.FilterChain;
@@ -12,9 +16,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+/**
+ * JWT 관련 인증을 처리해주는 필터입니다.
+ */
 public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
 
-    @Autowired
     private final AuthenticationService authenticationService;
 
     // authenticationManager : Spring Security의 필터들이 인증을 수행하는 방법에 대한 명세를 정의해 놓은 인터페이스
@@ -30,39 +36,18 @@ public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
                                     FilterChain chain)
             throws IOException, ServletException {
 
-        // TODO : authentication
-        if (filterWithPathAndMethod(request)) {
-            return;
-        }
-
         String authorization = request.getHeader("Authorization");
 
-        if (authorization == null) {
-            throw new InvalidTokenException("Invalid Token!");
+        if (authorization != null) {
+            String accessToken = authorization.substring("Bearer ".length());
+            Long userId = authenticationService.parseToken(accessToken);
+            Authentication authentication = new UserAuthentication(userId); // 권한 획득하는 곳
+
+            // SecurityContextHolder : Spring Security의 인메모리 세션저장소
+            SecurityContext context = SecurityContextHolder.getContext();
+            context.setAuthentication(authentication);
         }
-
-        String accessToken = authorization.substring("Bearer ".length());
-
-        authenticationService.parseToken(accessToken);
 
         chain.doFilter(request, response);
-    }
-
-    private boolean filterWithPathAndMethod(HttpServletRequest request) {
-        String path = request.getRequestURI();
-        if (!path.startsWith("/products")) {
-            return true;
-        }
-
-        String method = request.getMethod();
-        if (method.equals("GET")) {
-            return true;
-        }
-
-        if (method.equals("OPTIONS")) {
-            return true;
-        }
-
-        return false;
     }
 }
