c0e0311f02da65cb9a23299c05b15e5bc59c081b
Sungeheon <elon4856@gmail.com>
2021-09-27 19:19:36 +0900

    7-1강 반영

---
+++ app/src/main/java/com/codesoom/assignment/config/SecurityJavaConfig.java
@@ -0,0 +1,32 @@
+package com.codesoom.assignment.config;
+
+import com.codesoom.assignment.application.AuthenticationService;
+import com.codesoom.assignment.filters.AuthenticationErrorFilter;
+import com.codesoom.assignment.filters.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.servlet.Filter;
+
+@Configuration
+public class SecurityJavaConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private AuthenticationService authenticationService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        final Filter authenticationFilter = new JwtAuthenticationFilter(
+                authenticationManager(), authenticationService
+        );
+
+        final Filter authenticationErrorFilter = new AuthenticationErrorFilter();
+        http
+                .csrf().disable()
+                .addFilter(authenticationFilter)
+                .addFilterBefore(
+                        authenticationErrorFilter, JwtAuthenticationFilter.class
+                );
+    }
+}
