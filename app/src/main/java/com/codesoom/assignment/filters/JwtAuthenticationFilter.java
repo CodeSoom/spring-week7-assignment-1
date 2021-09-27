3836d6656047ffee33af9c2b383fad25df059391
bloomspes <bloomspes@gmail.com>
2021-09-27 20:33:47 +0900

    refactor : interceptors -> jwtAuthenticationFilter 인증 분리

--- app/src/main/java/com/codesoom/assignment/filters/JwtAuthenticationFilter.java
+++ app/src/main/java/com/codesoom/assignment/filters/JwtAuthenticationFilter.java
@@ -1,5 +1,7 @@
 package com.codesoom.assignment.filters;
 
+import com.codesoom.assignment.application.AuthenticationService;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
@@ -10,9 +12,13 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
+    private final AuthenticationService authenticationService;
+
     public JwtAuthenticationFilter(
-            AuthenticationManager authenticationManager) {
+            AuthenticationManager authenticationManager,
+            AuthenticationService authenticationService) {
         super(authenticationManager);
+        this.authenticationService = authenticationService;
     }
 
     @Override
@@ -21,7 +27,17 @@ public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
                                     FilterChain chain)
             throws IOException, ServletException {
         // TODO : authentication
+        String authorization = request.getHeader("Authorization");
 
+        if (authorization == null) {
+            response.sendError(HttpStatus.UNAUTHORIZED.value());
+            return;
+        }
+
+        String accessToken = authorization.substring("Bearer ".length());
+        Long userId = authenticationService.parseToken(accessToken);
+
+        request.setAttribute("userId", userId);
         chain.doFilter(request, response);
     }
 }
