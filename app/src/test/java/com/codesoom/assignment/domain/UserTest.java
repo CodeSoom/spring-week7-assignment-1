9d468ad938bdb2f7f665fc8039c2084ec8a3654c
Sungeheon <elon4856@gmail.com>
2021-09-30 20:11:45 +0900

    fix: 아이디 삭제시 authenticate실패한다는 결과를 명확히 하세요

--- app/src/test/java/com/codesoom/assignment/domain/UserTest.java
+++ app/src/test/java/com/codesoom/assignment/domain/UserTest.java
@@ -61,8 +61,13 @@ class UserTest {
 
     @Test
     void authenticateWithDeletedUser() {
-        User user = User.builder().deleted(true).build();
+        User user = User.builder().build();
         user.changePassword("test", passwordEncoder);
+
+        assertThat(user.authenticate("test", passwordEncoder)).isTrue();
+        assertThat(user.authenticate("xxx", passwordEncoder)).isFalse();
+
+        user.destroy();
 
         assertThat(user.authenticate("test", passwordEncoder)).isFalse();
         assertThat(user.authenticate("xxx", passwordEncoder)).isFalse();
