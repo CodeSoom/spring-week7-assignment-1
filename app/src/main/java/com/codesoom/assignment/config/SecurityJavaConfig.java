d57d412c5fffc700727af334ed5f585afc60c8f0
kiheo <sweet7257@naver.com>
2021-10-02 06:33:09 +0900

    스프링 7주차 과제 #2

--- app/src/main/java/com/codesoom/assignment/config/SecurityJavaConfig.java
+++ app/src/main/java/com/codesoom/assignment/config/SecurityJavaConfig.java
@@ -4,12 +4,15 @@ import com.codesoom.assignment.application.AuthenticationService;
 import filters.AuthenticationErrorFilter;
 import filters.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 import javax.servlet.Filter;
@@ -37,5 +40,10 @@ public class SecurityJavaConfig extends WebSecurityConfigurerAdapter {
                         new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)
                 );
 
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
