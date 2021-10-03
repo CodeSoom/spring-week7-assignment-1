8095e8e6b8cbd0c70c916ecaf1922b0729cddece
bloomspes <bloomspes@gmail.com>
2021-10-04 00:13:58 +0900

    과제 : UserService 코드 수정

--- app/src/test/java/com/codesoom/assignment/errors/UserForbiddenException.java
+++
@@ -1,8 +0,0 @@
-package com.codesoom.assignment.errors;
-
-public class UserForbiddenException extends RuntimeException {
-    public UserForbiddenException(Long id) {
-        super("Forbidden this page" + id);
-    }
-
-}
