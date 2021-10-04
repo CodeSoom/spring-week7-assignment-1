0668b60ef7374a463c11e325929dda5abded3ea8
pangnem <sprnd645@gmail.com>
2021-10-04 18:51:04 +0900

    style: 포매터 적용

--- app/src/test/java/com/codesoom/assignment/controllers/UserControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/UserControllerTest.java
@@ -1,5 +1,16 @@
 package com.codesoom.assignment.controllers;
 
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import com.codesoom.assignment.application.AuthenticationService;
 import com.codesoom.assignment.application.UserService;
 import com.codesoom.assignment.domain.User;
@@ -14,17 +25,9 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.verify;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 @WebMvcTest(UserController.class)
 class UserControllerTest {
+
     @Autowired
     private MockMvc mockMvc;
 
@@ -37,53 +40,52 @@ class UserControllerTest {
     @BeforeEach
     void setUp() {
         given(userService.registerUser(any(UserRegistrationData.class)))
-                .will(invocation -> {
-                    UserRegistrationData registrationData = invocation.getArgument(0);
-                    return User.builder()
-                            .id(13L)
-                            .email(registrationData.getEmail())
-                            .name(registrationData.getName())
-                            .build();
-                });
-
+            .will(invocation -> {
+                UserRegistrationData registrationData = invocation.getArgument(0);
+                return User.builder()
+                    .id(13L)
+                    .email(registrationData.getEmail())
+                    .name(registrationData.getName())
+                    .build();
+            });
 
         given(userService.updateUser(eq(1L), any(UserModificationData.class)))
-                .will(invocation -> {
-                    Long id = invocation.getArgument(0);
-                    UserModificationData modificationData =
-                            invocation.getArgument(1);
-                    return User.builder()
-                            .id(id)
-                            .email("tester@example.com")
-                            .name(modificationData.getName())
-                            .build();
-                });
+            .will(invocation -> {
+                Long id = invocation.getArgument(0);
+                UserModificationData modificationData =
+                    invocation.getArgument(1);
+                return User.builder()
+                    .id(id)
+                    .email("tester@example.com")
+                    .name(modificationData.getName())
+                    .build();
+            });
 
         given(userService.updateUser(eq(100L), any(UserModificationData.class)))
-                .willThrow(new UserNotFoundException(100L));
+            .willThrow(new UserNotFoundException(100L));
 
         given(userService.deleteUser(100L))
-                .willThrow(new UserNotFoundException(100L));
+            .willThrow(new UserNotFoundException(100L));
     }
 
     @Test
     void registerUserWithValidAttributes() throws Exception {
         mockMvc.perform(
-                post("/users")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"email\":\"tester@example.com\"," +
-                                "\"name\":\"Tester\",\"password\":\"test\"}")
+            post("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"email\":\"tester@example.com\"," +
+                    "\"name\":\"Tester\",\"password\":\"test\"}")
         )
-                .andExpect(status().isCreated())
-                .andExpect(content().string(
-                        containsString("\"id\":13")
-                ))
-                .andExpect(content().string(
-                        containsString("\"email\":\"tester@example.com\"")
-                ))
-                .andExpect(content().string(
-                        containsString("\"name\":\"Tester\"")
-                ));
+            .andExpect(status().isCreated())
+            .andExpect(content().string(
+                containsString("\"id\":13")
+            ))
+            .andExpect(content().string(
+                containsString("\"email\":\"tester@example.com\"")
+            ))
+            .andExpect(content().string(
+                containsString("\"name\":\"Tester\"")
+            ));
 
         verify(userService).registerUser(any(UserRegistrationData.class));
     }
@@ -91,27 +93,27 @@ class UserControllerTest {
     @Test
     void registerUserWithInvalidAttributes() throws Exception {
         mockMvc.perform(
-                post("/users")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content("{}")
+            post("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{}")
         )
-                .andExpect(status().isBadRequest());
+            .andExpect(status().isBadRequest());
     }
 
     @Test
     void updateUserWithValidAttributes() throws Exception {
         mockMvc.perform(
-                patch("/users/1")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"name\":\"TEST\",\"password\":\"test\"}")
+            patch("/users/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"name\":\"TEST\",\"password\":\"test\"}")
         )
-                .andExpect(status().isOk())
-                .andExpect(content().string(
-                        containsString("\"id\":1")
-                ))
-                .andExpect(content().string(
-                        containsString("\"name\":\"TEST\"")
-                ));
+            .andExpect(status().isOk())
+            .andExpect(content().string(
+                containsString("\"id\":1")
+            ))
+            .andExpect(content().string(
+                containsString("\"name\":\"TEST\"")
+            ));
 
         verify(userService).updateUser(eq(1L), any(UserModificationData.class));
     }
@@ -119,30 +121,30 @@ class UserControllerTest {
     @Test
     void updateUserWithInvalidAttributes() throws Exception {
         mockMvc.perform(
-                patch("/users/1")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"name\":\"\",\"password\":\"\"}")
+            patch("/users/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"name\":\"\",\"password\":\"\"}")
         )
-                .andExpect(status().isBadRequest());
+            .andExpect(status().isBadRequest());
     }
 
     @Test
     void updateUserWithNotExsitedId() throws Exception {
         mockMvc.perform(
-                patch("/users/100")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"name\":\"TEST\",\"password\":\"TEST\"}")
+            patch("/users/100")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"name\":\"TEST\",\"password\":\"TEST\"}")
         )
-                .andExpect(status().isNotFound());
+            .andExpect(status().isNotFound());
 
         verify(userService)
-                .updateUser(eq(100L), any(UserModificationData.class));
+            .updateUser(eq(100L), any(UserModificationData.class));
     }
 
     @Test
     void destroyWithExistedId() throws Exception {
         mockMvc.perform(delete("/users/1"))
-                .andExpect(status().isNoContent());
+            .andExpect(status().isNoContent());
 
         verify(userService).deleteUser(1L);
     }
@@ -150,7 +152,7 @@ class UserControllerTest {
     @Test
     void destroyWithNotExistedId() throws Exception {
         mockMvc.perform(delete("/users/100"))
-                .andExpect(status().isNotFound());
+            .andExpect(status().isNotFound());
 
         verify(userService).deleteUser(100L);
     }
