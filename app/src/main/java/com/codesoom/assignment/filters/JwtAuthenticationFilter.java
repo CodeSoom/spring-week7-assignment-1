b13814fc80aeec16c3d98521d00675e60d7662aa
jihwooon <jihwooon@gmail.com>
2022-01-11 00:44:05 +0900

    7주가 : 인가 (Authorization) 구현하기
    이번주 7주차 과제 올립니다.
    
    6주차 때 못 올린점 죄송합니다.
    
    개인적인 일로 6주차는 컷밋은 못 올리고 과제 문제 풀이로 해결 했습니다.
    
    이번 한 주 잘 부탁드리겠습니다.

---
+++ app/src/main/java/com/codesoom/assignment/filters/JwtAuthenticationFilter.java
@@ -0,0 +1,64 @@
+package com.codesoom.assignment.filters;
+
+import com.codesoom.assignment.application.AuthenticationService;
+import com.codesoom.assignment.errors.InvalidTokenException;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
+    private final AuthenticationService authenticationService;
+
+    public JwtAuthenticationFilter(
+            AuthenticationManager authenticationManager,
+            AuthenticationService authenticationService) {
+        super(authenticationManager);
+        this.authenticationService = authenticationService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain chain) throws IOException, ServletException {
+
+        if (filterWithPathAndMethod(request)) {
+            chain.doFilter(request,response);
+            return;
+        }
+
+        String authorization = request.getHeader("Authorization");
+
+        if (authorization == null) {
+            throw new InvalidTokenException("");
+        }
+
+        String accessToken = authorization.substring("Bearer ".length());
+        authenticationService.parseToken(accessToken);
+
+        chain.doFilter(request,response);
+    }
+
+    private boolean filterWithPathAndMethod(HttpServletRequest request) {
+        String path = request.getRequestURI();
+
+        if (!path.startsWith("/products")) {
+            return true;
+        }
+
+        String method = request.getMethod();
+        if (method.equals("GET")) {
+            return true;
+        }
+
+        if (method.equals("OPTIONS")) {
+            return true;
+        }
+
+        return false;
+    }
+}
