4e20473c3657b63c8b07ee00a973c82dfd5a60f7
kyuwon53 <lkw2v@naver.com>
2022-01-10 20:30:24 +0900

    [Create] Spring Security에 대한 설정을 하라

---
+++ app/src/main/java/com/codesoom/assignment/config/SecurityJavaConfig.java
@@ -0,0 +1,20 @@
+package com.codesoom.assignment.config;
+
+import com.codesoom.assignment.filters.JwtAuthenticationFilter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.servlet.Filter;
+
+@Configuration
+public class SecurityJavaConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        Filter authenticationFilter = new JwtAuthenticationFilter(
+                authenticationManager()
+        );
+        http.csrf().disable()
+                .addFilter(authenticationFilter);
+    }
+}
