55509450bc6139a736186a32b2b366e9b9ed16d0
Seungchul Jang <jangsc0000@gmail.com>
2022-01-11 16:43:12 +0900

    [리팩토링] Spring Security로 인증, 인가 처리

---
+++ app/src/main/java/com/codesoom/assignment/security/UserAuthentication.java
@@ -0,0 +1,39 @@
+package com.codesoom.assignment.security;
+
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class UserAuthentication extends AbstractAuthenticationToken {
+    private final Long userId;
+
+    public UserAuthentication(Long userId) {
+        super(authorities());
+        this.userId = userId;
+    }
+
+    private static List<GrantedAuthority> authorities() {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("USER"));
+        return authorities;
+    }
+
+    @Override
+    public boolean isAuthenticated() {
+        return true;
+    }
+
+    @Override
+    public Object getCredentials() {
+        return null;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return userId;
+    }
+}
