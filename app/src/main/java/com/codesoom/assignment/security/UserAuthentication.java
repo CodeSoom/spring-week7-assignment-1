eb6a701fcc82c1ceb2480cf38d6e9e18e1532bcb
hyuk <pjh08190819@naver.com>
2022-01-13 02:13:18 +0900

    docs : isAuthenticated 메서드 javadoc 수정

--- app/src/main/java/com/codesoom/assignment/security/UserAuthentication.java
+++ app/src/main/java/com/codesoom/assignment/security/UserAuthentication.java
@@ -37,9 +37,9 @@ public class UserAuthentication extends AbstractAuthenticationToken {
     }
 
     /**
-     * 인증 여부를 반환합니다.
+     * 인증이 되었으면 true를 반환하고, 인증이 안되었으면 false를 반환합니다.
      *
-     * @return boolean 인증 여부
+     * @return boolean 인증
      */
     @Override
     public boolean isAuthenticated() {
