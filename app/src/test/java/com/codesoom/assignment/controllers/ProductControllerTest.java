ce7fbde2a932e3b13ba026bd07efd338c401a661
hyuk <pjh08190819@naver.com>
2022-01-12 11:55:37 +0900

    REFACTOR : Spring Security 수강 후 수정

--- app/src/test/java/com/codesoom/assignment/controllers/ProductControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/ProductControllerTest.java
@@ -265,4 +265,16 @@ class ProductControllerTest {
         )
                 .andExpect(status().isUnauthorized());
     }
+
+    @Test
+    void destroyWithoutAccessToken() throws Exception {
+        mockMvc.perform(
+                        patch("/products/1")
+                                .accept(MediaType.APPLICATION_JSON_UTF8)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("{\"name\":\"쥐순이\",\"maker\":\"냥이월드\"," +
+                                        "\"price\":5000}")
+                )
+                .andExpect(status().isUnauthorized());
+    }
 }
