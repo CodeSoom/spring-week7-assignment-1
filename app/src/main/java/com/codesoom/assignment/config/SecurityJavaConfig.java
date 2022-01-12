d1adff0a829117413889e2a8a3990430ce347aeb
Kwakcena <rhkrgudwh@gmail.com>
2022-01-12 22:16:04 +0900

    csrf 기능을 끕니다
    Security의 csrf 기능을 사용하지 않습니다.

--- app/src/main/java/com/codesoom/assignment/config/SecurityJavaConfig.java
+++ app/src/main/java/com/codesoom/assignment/config/SecurityJavaConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityJavaConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http
+                .csrf().disable();
     }
 }
