352823ac1374fef58ae1b06115361e645dfcfb06
kyuwon53 <lkw2v@naver.com>
2022-01-13 18:06:25 +0900

    [Add] 사용자 인증 javadoc을 추가하라

--- app/src/main/java/com/codesoom/assignment/security/UserAuthentication.java
+++ app/src/main/java/com/codesoom/assignment/security/UserAuthentication.java
@@ -1,5 +1,6 @@
 package com.codesoom.assignment.security;
 
+import lombok.Getter;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -7,38 +8,56 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 회원 인증정보를 표현합니다.
+ */
 public class UserAuthentication extends AbstractAuthenticationToken {
+    @Getter
+    private String accessToken;
 
-    private final Long userId;
+    private Long userId;
 
-    public UserAuthentication(Long userId) {
-        super(authorities());
+    public UserAuthentication(Roles role, Long userId, String accessToken) {
+        super(authorities(role));
         this.userId = userId;
+        this.accessToken = accessToken;
     }
 
-    private static List<GrantedAuthority> authorities() {
-        List<GrantedAuthority> authorities = new ArrayList<>();
-        authorities.add(new SimpleGrantedAuthority("USER"));
-        return null;
+    public UserAuthentication(Roles role, String accessToken) {
+        super(authorities(role));
+        this.accessToken = accessToken;
     }
 
+    /**
+     * 사용자 인증 키를 리턴합니다.
+     *
+     * @return 토큰
+     */
     @Override
     public Object getCredentials() {
-        return null;
+        return accessToken;
     }
 
-    @Override
-    public String toString() {
-        return "Authentication (" + userId + ')';
-    }
-
-    @Override
-    public boolean isAuthenticated() {
-        return true;
-    }
-
+    /**
+     * 사용자 아이디를 리턴합니다.
+     *
+     * @return 사용자 아이디
+     */
     @Override
     public Object getPrincipal() {
         return userId;
+    }
+
+    /**
+     * 사용자 권한을 설정하고 리턴합니다.
+     *
+     * @param role 회원 권한
+     * @return 권한 목록
+     */
+    private static List<GrantedAuthority> authorities(Roles role) {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+
+        authorities.add(new SimpleGrantedAuthority(role.getRole()));
+        return authorities;
     }
 }
