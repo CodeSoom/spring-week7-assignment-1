59f4209450175d85d228122831df2ca14b958338
kyuwon53 <lkw2v@naver.com>
2022-01-13 17:02:22 +0900

    [Add] 사용자 컨트롤러에 대한 javadoc을 작성하라

--- app/src/main/java/com/codesoom/assignment/controllers/UserController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/UserController.java
@@ -6,10 +6,21 @@ import com.codesoom.assignment.dto.UserModificationData;
 import com.codesoom.assignment.dto.UserRegistrationData;
 import com.codesoom.assignment.dto.UserResultData;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
+/**
+ * 사용자에 대한 HTTP 요청의 처리를 담당한다.
+ */
 @RestController
 @RequestMapping("/users")
 @CrossOrigin
@@ -20,6 +31,12 @@ public class UserController {
         this.userService = userService;
     }
 
+    /**
+     * 사용자를 생성하고 리턴한다.
+     *
+     * @param registrationData 사용자 생성 정보
+     * @return 생성된 사용자
+     */
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     UserResultData create(@RequestBody @Valid UserRegistrationData registrationData) {
@@ -27,6 +44,13 @@ public class UserController {
         return getUserResultData(user);
     }
 
+    /**
+     * 요청받은 id의 사용자 정보를 수정한다.
+     *
+     * @param id               수정할 사용자 id
+     * @param modificationData 사용자의 수정된 정보
+     * @return 수정된 사용자
+     */
     @PatchMapping("{id}")
     UserResultData update(
             @PathVariable Long id,
@@ -36,6 +60,11 @@ public class UserController {
         return getUserResultData(user);
     }
 
+    /**
+     * 요청받은 id의 사용자를 삭제한다.
+     *
+     * @param id 삭제할 사용자 id
+     */
     @DeleteMapping("{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     void destroy(@PathVariable Long id) {
