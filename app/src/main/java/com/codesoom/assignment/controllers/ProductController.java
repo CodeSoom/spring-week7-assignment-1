05dc266a23768b696a32cb3dc3cec309c32b3730
kyuwon53 <lkw2v@naver.com>
2022-01-13 16:52:08 +0900

    [Add] 상품컨트롤러에 대한 javadoc을 완성하라

--- app/src/main/java/com/codesoom/assignment/controllers/ProductController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/ProductController.java
@@ -10,12 +10,23 @@ import com.codesoom.assignment.domain.Product;
 import com.codesoom.assignment.dto.ProductData;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.util.List;
 
+/**
+ * 상품에 대한 HTTP 요청의 처리를 담당한다.
+ */
 @RestController
 @RequestMapping("/products")
 @CrossOrigin
@@ -30,42 +41,67 @@ public class ProductController {
         this.authenticationService = authenticationService;
     }
 
+    /**
+     * 상품목록을 리턴한다.
+     *
+     * @return 상품목록
+     */
     @GetMapping
     public List<Product> list() {
         return productService.getProducts();
     }
 
+    /**
+     * 요청된 id의 상품을 리턴한다.
+     *
+     * @param id 상품id
+     * @return 해당하는 상품
+     */
     @GetMapping("{id}")
     public Product detail(@PathVariable Long id) {
         return productService.getProduct(id);
     }
 
+    /**
+     * 상품을 생성하고 리턴한다.
+     *
+     * @param productData 생성할 상품
+     * @return 생성된 상품
+     */
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     @PreAuthorize("isAuthenticated() and hasAuthenrity('USER')")
     public Product create(
-            @RequestAttribute Long userId,
-            @RequestBody @Valid ProductData productData,
-            Authentication authentication
+            @RequestBody @Valid ProductData productData
     ) {
         return productService.createProduct(productData);
     }
 
+    /**
+     * 요청한 상품을 수정하고 리턴한다.
+     *
+     * @param productData 상품의 수정된 정보
+     * @param id          수정할 상품의 id
+     * @return 수정된 상품
+     */
     @PatchMapping("{id}")
     @PreAuthorize("isAuthenticated()")
     public Product update(
-            @RequestAttribute Long userId,
             @PathVariable Long id,
             @RequestBody @Valid ProductData productData
     ) {
         return productService.updateProduct(id, productData);
     }
 
+    /**
+     * 요청한 상품을 삭제한다.
+     *
+     * @param id 삭제할 상품의 id
+     */
     @DeleteMapping("{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PreAuthorize("isAuthenticated()")
     public void destroy(
-            @RequestAttribute Long userId,
             @PathVariable Long id
     ) {
         productService.deleteProduct(id);
