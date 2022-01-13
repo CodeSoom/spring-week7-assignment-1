1f51425b82d1f3898f64236834928a728fcdb062
kyuwon53 <lkw2v@naver.com>
2022-01-13 17:17:46 +0900

    [Add] 사용자 javadoc을 작성하라

--- app/src/main/java/com/codesoom/assignment/domain/User.java
+++ app/src/main/java/com/codesoom/assignment/domain/User.java
@@ -10,6 +10,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+/**
+ * 사용자 정보를 관리한다.
+ */
 @Entity
 @Getter
 @Builder
@@ -32,19 +35,41 @@ public class User {
     @Builder.Default
     private boolean deleted = false;
 
+    /**
+     * 사용자 정보를 수정한다.
+     *
+     * @param source 사용자 수정 정보
+     */
     public void changeWith(User source) {
         name = source.name;
+        password = source.password;
     }
 
+    /**
+     * 비밀번호를 변경합니다.
+     *
+     * @param password        변경할 비밀번호
+     * @param passwordEncoder 비밀번호 인코더
+     */
     public void changePassword(String password,
                                PasswordEncoder passwordEncoder) {
         this.password = passwordEncoder.encode(password);
     }
 
+    /**
+     * 사용자를 탈퇴 처리한다.
+     */
     public void destroy() {
         deleted = true;
     }
 
+    /**
+     * 사용자 정보를 인증하고 리턴한다.
+     *
+     * @param password        비밀번호
+     * @param passwordEncoder 비밀번호 인코더
+     * @return 인증결과
+     */
     public boolean authenticate(String password
             , PasswordEncoder passwordEncoder
     ) {
