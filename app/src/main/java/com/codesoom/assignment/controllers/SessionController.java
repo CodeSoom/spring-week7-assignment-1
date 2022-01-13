072ce8cf5b9fe50fe91b5506b699a299c2f6bbb8
kyuwon53 <lkw2v@naver.com>
2022-01-13 16:59:22 +0900

    [Add] 사용자 인증 컨트롤러 javadoc을 추가하라

--- app/src/main/java/com/codesoom/assignment/controllers/SessionController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/SessionController.java
@@ -4,8 +4,16 @@ import com.codesoom.assignment.application.AuthenticationService;
 import com.codesoom.assignment.dto.SessionRequestData;
 import com.codesoom.assignment.dto.SessionResponseData;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 사용자 인증에 대한 HTTP 요청을 처리한다.
+ */
 @RestController
 @RequestMapping("/session")
 @CrossOrigin
@@ -16,6 +24,12 @@ public class SessionController {
         this.authenticationService = authenticationService;
     }
 
+    /**
+     * 사용자를 인증하고 결과를 리턴한다.
+     *
+     * @param sessionRequestData 사용자 인증 정보
+     * @return 인증 결과
+     */
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public SessionResponseData login(
