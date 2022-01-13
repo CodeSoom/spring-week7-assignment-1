d8606710af60034d00487d0a188c66515736067a
kyuwon53 <lkw2v@naver.com>
2022-01-13 18:06:01 +0900

    [Add] role getter 설정

--- app/src/main/java/com/codesoom/assignment/security/Roles.java
+++ app/src/main/java/com/codesoom/assignment/security/Roles.java
@@ -1,5 +1,7 @@
 package com.codesoom.assignment.security;
 
+import lombok.Getter;
+
 /**
  * 회원 권한을 정의합니다.
  */
@@ -7,7 +9,7 @@ public enum Roles {
     ANONYMOUS("ANONYMOUS"),
     ADMIN("ADMIN"),
     USER("USER");
-
+    @Getter
     private final String role;
 
     Roles(String role) {
