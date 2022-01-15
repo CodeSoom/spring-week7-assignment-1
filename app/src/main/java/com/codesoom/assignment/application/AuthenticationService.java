1fcf495562e91cab76a0a700a67b038a9c5ec409
hyuk <pjh08190819@naver.com>
2022-01-16 03:20:27 +0900

    feat : AuthenticationService 에서 권한 리스트 리턴하는 로직 구현

--- app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
+++ app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
@@ -1,5 +1,7 @@
 package com.codesoom.assignment.application;
 
+import com.codesoom.assignment.domain.Role;
+import com.codesoom.assignment.domain.RoleRepository;
 import com.codesoom.assignment.domain.User;
 import com.codesoom.assignment.domain.UserRepository;
 import com.codesoom.assignment.errors.LoginFailException;
@@ -8,16 +10,22 @@ import io.jsonwebtoken.Claims;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class AuthenticationService {
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
     private final JwtUtil jwtUtil;
     private final PasswordEncoder passwordEncoder;
 
     public AuthenticationService(UserRepository userRepository,
+                                 RoleRepository roleRepository,
                                  JwtUtil jwtUtil,
                                  PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
         this.jwtUtil = jwtUtil;
         this.passwordEncoder = passwordEncoder;
     }
@@ -36,5 +44,9 @@ public class AuthenticationService {
     public Long parseToken(String accessToken) {
         Claims claims = jwtUtil.decode(accessToken);
         return claims.get("userId", Long.class);
+    }
+
+    public List<Role> roles(Long userId) {
+        return roleRepository.findAllByUserId(userId);
     }
 }
---
+++ app/src/main/java/com/codesoom/assignment/domain/Role.java
@@ -0,0 +1,39 @@
+package com.codesoom.assignment.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private Long userId;
+
+    @Getter
+    private String name;
+
+    public Role(Long userId, String name) {
+        this.userId = userId;
+        this.name = name;
+    }
+
+    public Role(String name) {
+        this(null, name);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
