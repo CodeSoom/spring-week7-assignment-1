34e08729331883e2633ce205678ee160a056b4db
hyuk <pjh08190819@naver.com>
2022-01-16 03:30:04 +0900

    refactor : 유저 업데이트 시 USER 권한 체크

--- app/src/main/java/com/codesoom/assignment/controllers/UserController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/UserController.java
@@ -32,7 +32,7 @@ public class UserController {
     }
 
     @PatchMapping("{id}")
-    @PreAuthorize("isAuthenticated()")
+    @PreAuthorize("isAuthenticated() and hasAuthority('USER')")
     UserResultData update(
             @PathVariable Long id,
             @RequestBody @Valid UserModificationData modificationData,
