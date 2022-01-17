19f2eae40072cf3b90a0ca35a4543063265e0ce9
hyuk <pjh08190819@naver.com>
2022-01-17 22:25:50 +0900

    feat : role 클래스 id에 identity 전략 추가

--- app/src/main/java/com/codesoom/assignment/domain/Role.java
+++ app/src/main/java/com/codesoom/assignment/domain/Role.java
@@ -5,6 +5,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
@@ -12,7 +13,7 @@ import javax.persistence.Id;
 public class Role {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private Long userId;
