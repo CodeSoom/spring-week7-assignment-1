e8bf36bf364303c925222425f1dc7d44830336f0
shineoov <shineoov88@gmail.com>
2022-05-03 00:57:02 +0900

    test: User 클래스 비밀번호 변경 메소드 테스트

--- app/src/test/java/com/codesoom/assignment/domain/UserTest.java
+++ app/src/test/java/com/codesoom/assignment/domain/UserTest.java
@@ -1,10 +1,56 @@
 package com.codesoom.assignment.domain;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@DisplayName("User 클래스")
 class UserTest {
+
+    private static final String ORIGIN_PASSWORD = "1111";
+    private static final String CHANGE_PASSWORD = "2222";
+
+    @Nested
+    @DisplayName("changePassword 메소드는")
+    class Describe_changePassword {
+
+        final User user = User.builder()
+                .password(ORIGIN_PASSWORD)
+                .build();
+
+        @Nested
+        @DisplayName("PasswordEncoder 를 활용하여")
+        class Context_usePasswordEncoder {
+
+            PasswordEncoder passwordEncoder;
+
+            @BeforeEach
+            void setUp() {
+                passwordEncoder = new BCryptPasswordEncoder();
+            }
+
+            void subject() {
+                user.changePassword(CHANGE_PASSWORD, passwordEncoder);
+            }
+
+            @Test
+            @DisplayName("비밀번호를 암호화해서 변경한다.")
+            void it_changeEncryptPassword() {
+
+                subject();
+
+                boolean isPasswordMatch = passwordEncoder.matches(CHANGE_PASSWORD, user.getPassword());
+
+                assertThat(isPasswordMatch).isTrue();
+            }
+        }
+    }
+
     @Test
     void changeWith() {
         User user = User.builder().build();
