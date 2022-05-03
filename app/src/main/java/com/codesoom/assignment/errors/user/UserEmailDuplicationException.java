bd361a357cfa49931922388a1b9dc344ba1e81a5
Lee Byoung Seong <dev.bslee@gmail.com>
2022-05-03 20:13:56 +0900

    refactor:중복된 유저 에러메세지 하드 코딩 제거

--- app/src/main/java/com/codesoom/assignment/errors/user/UserEmailDuplicationException.java
+++ app/src/main/java/com/codesoom/assignment/errors/user/UserEmailDuplicationException.java
@@ -1,7 +1,9 @@
 package com.codesoom.assignment.errors.user;
 
+import com.codesoom.assignment.common.message.ErrorMessage;
+
 public class UserEmailDuplicationException extends RuntimeException {
     public UserEmailDuplicationException(String email) {
-        super("User email is alreay existed: " + email);
+        super(ErrorMessage.EMAIL_IS_DUPLICATE.getErrorMsg());
     }
 }
