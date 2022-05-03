110bf663ff45d07d9576c2d3bf0c4bc6976ebb4c
Lee Byoung Seong <dev.bslee@gmail.com>
2022-05-03 20:06:21 +0900

    feature:비밀번호 공백 에러 메세지 추가

--- app/src/main/java/com/codesoom/assignment/common/message/ErrorMessage.java
+++ app/src/main/java/com/codesoom/assignment/common/message/ErrorMessage.java
@@ -1,10 +1,11 @@
-package com.codesoom.assignment.errors.message;
+package com.codesoom.assignment.common.message;
 
 //TODO:다국어 처리 고민
 public enum ErrorMessage {
-    EMAIL_IS_EMPTY("email은 공백이 허용되지 않습니다"),
+    EMAIL_IS_EMPTY("이메일은 공백이 허용되지 않습니다"),
     NAME_IS_EMPTY("이름은 공백이 허용되지 않습니다"),
-    PASSWORD_LENGTH_PREFIX("비밀번호는"),
+    PASSWORD_IS_EMPTY("비밀번호는 공백이 허용되지 않습니다"),
+    PASSWORD_LENGTH_PREFIX("비밀번호는 "),
     PASSWORD_LENGTH_BELOW_SUFFIX("자 이하 이여야합니다"),
     PASSWORD_LENGTH_MORE_THAN_SUFFIX("자 이상 이어햐합니다");
 
