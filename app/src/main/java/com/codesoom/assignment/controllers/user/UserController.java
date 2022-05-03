43c441351b77084dc87fef3fb978cbc70266e657
Lee Byoung Seong <dev.bslee@gmail.com>
2022-05-03 20:07:13 +0900

    feature:User 가입 controller 구현

--- app/src/main/java/com/codesoom/assignment/controllers/user/UserController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/user/UserController.java
@@ -1,10 +1,12 @@
 package com.codesoom.assignment.controllers.user;
 
-import com.codesoom.assignment.application.user.UserService;
-import com.codesoom.assignment.domain.User;
-import com.codesoom.assignment.dto.UserModificationData;
-import com.codesoom.assignment.dto.UserRegistrationData;
-import com.codesoom.assignment.dto.UserResultData;
+import com.codesoom.assignment.application.user.UserServiceImpl;
+import com.codesoom.assignment.common.CommonResponse;
+import com.codesoom.assignment.common.message.NormalMessage;
+import com.codesoom.assignment.domain.user.User;
+import com.codesoom.assignment.dto.user.UserModificationData;
+import com.codesoom.assignment.dto.user.UserRegistrationData;
+import com.codesoom.assignment.dto.user.UserResultData;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,17 +16,17 @@ import javax.validation.Valid;
 @RequestMapping("/users")
 @CrossOrigin
 public class UserController {
-    private final UserService userService;
+    private final UserServiceImpl userService;
 
-    public UserController(UserService userService) {
+    public UserController(UserServiceImpl userService) {
         this.userService = userService;
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    UserResultData create(@RequestBody @Valid UserRegistrationData registrationData) {
-        User user = userService.registerUser(registrationData);
-        return getUserResultData(user);
+    CommonResponse create(@RequestBody @Valid UserRegistrationData registrationData) {
+        userService.join(registrationData);
+        return CommonResponse.success(NormalMessage.JOIN_OK.getNormalMsg());
     }
 
     @PatchMapping("{id}")
@@ -32,14 +34,15 @@ public class UserController {
             @PathVariable Long id,
             @RequestBody @Valid UserModificationData modificationData
     ) {
-        User user = userService.updateUser(id, modificationData);
-        return getUserResultData(user);
+//        User user = userService.updateUser(id, modificationData);
+//        return getUserResultData(user);
+        return null;
     }
 
     @DeleteMapping("{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     void destroy(@PathVariable Long id) {
-        userService.deleteUser(id);
+        //userService.deleteUser(id);
     }
 
     private UserResultData getUserResultData(User user) {
