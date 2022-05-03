47dce221c085069f1a1596115d22f3aa9c231e91
Lee Byoung Seong <dev.bslee@gmail.com>
2022-05-03 19:18:32 +0900

    feature:응답 공통단 생성

---
+++ app/src/main/java/com/codesoom/assignment/common/CommonResponse.java
@@ -0,0 +1,113 @@
+package com.codesoom.assignment.common;
+
+
+import com.codesoom.assignment.errors.message.ErrorMessage;
+import com.codesoom.assignment.domain.Builder;
+
+public class CommonResponse {
+    private Result result;
+    private Object data;
+    private Object message;
+    private String errorCode;
+
+    public static <T> CommonResponse success(T data, Object message) {
+        return new CommonResponseBuilder().result(Result.SUCCESS)
+                .data(data)
+                .message(message)
+                .build();
+    }
+
+    public static <T> CommonResponse success(T data) {
+        return success(data, null);
+    }
+
+    public static <T> CommonResponse success(String message) {
+        return success(null, message);
+    }
+
+    public static CommonResponse fail(Object message, String errorCode) {
+        return new CommonResponseBuilder().result(Result.FAIL)
+                .message(message)
+                .errorCode(errorCode)
+                .build();
+    }
+
+    public static CommonResponse fail(ErrorMessage errorCode) {
+
+        return new CommonResponseBuilder().result(Result.FAIL)
+                .message(errorCode.getErrorMsg())
+                .errorCode(errorCode.name())
+                .build();
+    }
+
+    public static CommonResponse fail(String errorCode) {
+        return new CommonResponseBuilder().result(Result.FAIL)
+                .errorCode(errorCode)
+                .build();
+    }
+
+    public enum Result {
+        SUCCESS, FAIL
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public Object getMessage() {
+        return message;
+    }
+
+    public Result getResult() {
+        return result;
+    }
+
+    public String getErrorCode() {
+        return errorCode;
+    }
+
+    private CommonResponse(CommonResponseBuilder commonResponseBuilder) {
+        this.result = commonResponseBuilder.result;
+        this.data = commonResponseBuilder.data;
+        this.message = commonResponseBuilder.message;
+        this.errorCode = commonResponseBuilder.errorCode;
+    }
+
+    public static class CommonResponseBuilder implements Builder<CommonResponse> {
+        private Result result;
+        private Object data;
+        private Object message;
+        private String errorCode;
+
+        private CommonResponseBuilder() {
+        }
+
+        public CommonResponseBuilder data(Object data) {
+            this.data = data;
+            return this;
+        }
+
+        public CommonResponseBuilder result(Result result) {
+            this.result = result;
+            return this;
+        }
+
+        public CommonResponseBuilder message(Object message) {
+            this.message = message;
+            return this;
+
+        }
+
+        public CommonResponseBuilder errorCode(String errorCode) {
+            this.errorCode = errorCode;
+            return this;
+        }
+
+        @Override
+        public CommonResponse build() {
+            return new CommonResponse(this);
+        }
+    }
+
+
+}
