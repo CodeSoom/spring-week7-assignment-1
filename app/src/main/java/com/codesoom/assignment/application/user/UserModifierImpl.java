4f090fe10ee9b9fa2d7b371ced280c2223f17b3c
Lee Byoung Seong <dev.bslee@gmail.com>
2022-05-03 20:05:59 +0900

    feature:유저 가입 구현

--- app/src/main/java/com/codesoom/assignment/application/user/UserModifierImpl.java
+++ app/src/main/java/com/codesoom/assignment/application/user/UserModifierImpl.java
@@ -1,16 +1,39 @@
 package com.codesoom.assignment.application.user;
 
+import com.codesoom.assignment.domain.crypt.CryptService;
 import com.codesoom.assignment.domain.user.User;
 import com.codesoom.assignment.domain.user.UserModifier;
+import com.codesoom.assignment.domain.user.UserModifierValidator;
 import com.codesoom.assignment.domain.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component
-public class UserModifierImpl implements UserModifier {
-    public final UserRepository userRepository;
+import javax.transaction.Transactional;
 
-    public UserModifierImpl(UserRepository userRepository) {
+@Component
+@Transactional
+public class UserModifierImpl implements UserModifier {
+    private UserRepository userRepository;
+    private UserModifierValidator userModifierValidator;
+    private CryptService cryptService;
+
+    @Autowired
+    public UserModifierImpl(
+            UserRepository userRepository,
+            UserModifierValidator userModifierValidator,
+            CryptService cryptService
+    ) {
         this.userRepository = userRepository;
+        this.userModifierValidator = userModifierValidator;
+        this.cryptService = cryptService;
+    }
+
+    public UserModifierImpl(
+            UserModifierValidator userModifierValidator,
+            CryptService cryptService
+    ) {
+        this.userModifierValidator = userModifierValidator;
+        this.cryptService = cryptService;
     }
 
     @Override
@@ -24,7 +47,9 @@ public class UserModifierImpl implements UserModifier {
     }
 
     @Override
-    public String join(User user) {
-        return null;
+    public void join(User user) {
+        userModifierValidator.joinValidator(user);
+        user = user.passwordEncode(cryptService);
+        userRepository.save(user);
     }
 }
--- app/src/main/java/com/codesoom/assignment/dto/user/UserRegistrationData.java
+++ app/src/main/java/com/codesoom/assignment/dto/user/UserRegistrationData.java
@@ -3,12 +3,11 @@ package com.codesoom.assignment.dto.user;
 import com.codesoom.assignment.common.vaildator.Email;
 import com.codesoom.assignment.common.vaildator.Name;
 import com.codesoom.assignment.common.vaildator.Password;
+import com.codesoom.assignment.domain.user.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
 
 @Getter
 @Builder
@@ -23,4 +22,13 @@ public class UserRegistrationData {
 
     @Password
     private String password;
+
+    public User toEntity() {
+        return User.UserBuilder
+                .builder()
+                .email(email)
+                .name(name)
+                .password(password)
+                .build();
+    }
 }
