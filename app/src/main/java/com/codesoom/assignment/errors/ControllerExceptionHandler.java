5c2dad1293847fe8491d0c9d9f90a85b9ae8af9d
Lee Byoung Seong <dev.bslee@gmail.com>
2022-05-03 20:14:54 +0900

    feature:중복 유저 error handler구현

--- app/src/main/java/com/codesoom/assignment/errors/ControllerExceptionHandler.java
+++ app/src/main/java/com/codesoom/assignment/errors/ControllerExceptionHandler.java
@@ -2,6 +2,7 @@ package com.codesoom.assignment.errors;
 
 
 import com.codesoom.assignment.common.CommonResponse;
+import com.codesoom.assignment.errors.user.UserEmailDuplicationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -17,9 +18,16 @@ public class ControllerExceptionHandler {
 
 
     @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(UserEmailDuplicationException.class)
+    public CommonResponse onUserEmailDuplicationException(RuntimeException e){
+        return CommonResponse.fail(e.getMessage());
+    }
+
+    @ResponseBody
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(ConstraintViolationException.class)
-    public CommonResponse onCustomInternalServerError(ConstraintViolationException e) {
+    public CommonResponse onConstraintViolationException(ConstraintViolationException e) {
         return null;
     }
 }
