55dc75222a1a236e01622a723c3f0ce14e23b44e
Lee Byoung Seong <dev.bslee@gmail.com>
2022-05-03 15:53:25 +0900

    refactor:user domain builder 구현

--- app/src/main/java/com/codesoom/assignment/domain/user/User.java
+++ app/src/main/java/com/codesoom/assignment/domain/user/User.java
@@ -1,9 +1,7 @@
 package com.codesoom.assignment.domain.user;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
+import com.codesoom.assignment.domain.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,22 +9,21 @@ import javax.persistence.Id;
 
 @Entity
 @Getter
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class User {
     @Id
     @GeneratedValue
     private Long id;
-
     private String email;
-
     private String name;
-
     private String password;
-
-    @Builder.Default
     private boolean deleted = false;
+
+    private User(UserBuilder userBuilder) {
+        this.id = id;
+        this.email = userBuilder.email;
+        this.name = userBuilder.name;
+        this.password = userBuilder.password;
+    }
 
     public void changeWith(User source) {
         name = source.name;
@@ -39,5 +36,41 @@ public class User {
 
     public boolean authenticate(String password) {
         return !deleted && password.equals(this.password);
+    }
+
+    public static class UserBuilder implements Builder<User> {
+        private Long id;
+        private String email;
+        private String name;
+        private String password;
+
+        public static UserBuilder builder() {
+            return new UserBuilder();
+        }
+
+        public UserBuilder id(long id) {
+            this.id = id;
+            return this;
+        }
+
+        public UserBuilder email(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public UserBuilder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public UserBuilder password(String password) {
+            this.password = password;
+            return this;
+        }
+
+        @Override
+        public User build() {
+            return new User(this);
+        }
     }
 }
