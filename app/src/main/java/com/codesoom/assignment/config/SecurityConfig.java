0ca37a24a188924f1e65f53498de39448be8ecb1
subin606 <gaemilee606@gmail.com>
2022-05-04 15:47:48 +0900

    PasswordEncoder 빈 등록

--- app/src/main/java/com/codesoom/assignment/config/SecurityConfig.java
+++ app/src/main/java/com/codesoom/assignment/config/SecurityConfig.java
@@ -3,13 +3,15 @@ package com.codesoom.assignment.config;
 import com.codesoom.assignment.application.auth.AuthorizationService;
 import com.codesoom.assignment.security.filter.AuthenticationErrorFilter;
 import com.codesoom.assignment.security.filter.JwtAuthenticationFilter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 import javax.servlet.Filter;
@@ -43,6 +45,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private Filter authenticationErrorFilter() {
         return new AuthenticationErrorFilter();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
