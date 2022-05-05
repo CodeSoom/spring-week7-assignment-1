3083cf89d86d2821598d7f785db3f38c450367b6
Lee Byoung Seong <dev.bslee@gmail.com>
2022-05-05 20:11:06 +0900

    @valid 유효성 검증 실패시 공통 메세지 handler위한 기능 버그 수정

--- app/src/main/java/com/codesoom/assignment/errors/ControllerExceptionHandler.java
+++ app/src/main/java/com/codesoom/assignment/errors/ControllerExceptionHandler.java
@@ -13,11 +13,12 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @Slf4j
-@ControllerAdvice
+@RestControllerAdvice
 public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
 
 
