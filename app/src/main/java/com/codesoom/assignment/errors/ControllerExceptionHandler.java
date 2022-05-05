2a9f28a5911a447b786fabd66852a75cbe5d6740
Lee Byoung Seong <dev.bslee@gmail.com>
2022-05-05 18:59:34 +0900

    @valid 유효성 검증 실패시 공통 메세지 handler위한 기능 구현

--- app/src/main/java/com/codesoom/assignment/errors/ControllerExceptionHandler.java
+++ app/src/main/java/com/codesoom/assignment/errors/ControllerExceptionHandler.java
@@ -4,30 +4,44 @@ package com.codesoom.assignment.errors;
 import com.codesoom.assignment.common.CommonResponse;
 import com.codesoom.assignment.errors.user.UserEmailDuplicationException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-
-import javax.validation.ConstraintViolationException;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @Slf4j
 @ControllerAdvice
-public class ControllerExceptionHandler {
+public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
 
 
-    @ResponseBody
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(UserEmailDuplicationException.class)
-    public CommonResponse onUserEmailDuplicationException(RuntimeException e){
+    public CommonResponse onUserEmailDuplicationException(RuntimeException e) {
         return CommonResponse.fail(e.getMessage());
     }
 
-    @ResponseBody
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    @ExceptionHandler(ConstraintViolationException.class)
-    public CommonResponse onConstraintViolationException(ConstraintViolationException e) {
-        return null;
+    @Override
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException exception,
+            HttpHeaders headers,
+            HttpStatus status,
+            WebRequest request) {
+
+        BindingResult bindingResult = exception.getBindingResult();
+
+        StringBuilder errorMessageBuilder = new StringBuilder();
+        for (FieldError fieldError : bindingResult.getFieldErrors()) {
+            errorMessageBuilder.append(fieldError.getDefaultMessage());
+        }
+
+        return new ResponseEntity(CommonResponse.fail(errorMessageBuilder.toString()), headers, status);
     }
 }
