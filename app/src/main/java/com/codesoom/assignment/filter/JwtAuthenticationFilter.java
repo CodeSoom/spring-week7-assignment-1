7fd56292474dcc246dbe0e118fb91b0d64aef281
shineoov <shineoov88@gmail.com>
2022-05-07 18:03:54 +0900

    JwtAuthenticationFilter 인가 정책에 맞게 수정

--- app/src/main/java/com/codesoom/assignment/filter/JwtAuthenticationFilter.java
+++ app/src/main/java/com/codesoom/assignment/filter/JwtAuthenticationFilter.java
@@ -2,6 +2,7 @@ package com.codesoom.assignment.filter;
 
 import com.codesoom.assignment.application.AuthenticationService;
 import com.codesoom.assignment.errors.InvalidTokenException;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
@@ -12,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
- *  JWT 인증 필터
+ * JWT 인증 필터
  */
 public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
 
@@ -28,6 +29,11 @@ public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
                                     HttpServletResponse response,
                                     FilterChain chain) throws IOException, ServletException {
 
+        if (!isAuthenticationRequired(request)) {
+            chain.doFilter(request, response);
+            return;
+        }
+
         final String authorization = request.getHeader("Authorization");
 
         if (authorization == null) {
@@ -39,5 +45,31 @@ public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
         authenticationService.parseToken(accessToken);
 
         chain.doFilter(request, response);
+    }
+
+    /**
+     * 인증이 필요한 HTTP 요청인지 확인합니다.<br>
+     * 인증이 필요한 요청이면 true 를 리턴하고, 인증이 필요하지 않다면 false 를 리턴합니다.<br><br>
+     *
+     * @param httpRequest HTTP 요청 정보
+     * @return 인증이 필요한 요청이면 true, 필요하지 않다면 false
+     */
+    private boolean isAuthenticationRequired(HttpServletRequest httpRequest) {
+
+        final String path = httpRequest.getRequestURI();
+        final String method = httpRequest.getMethod();
+
+        if (path.startsWith("/products") && HttpMethod.GET.matches(method)) {
+            return false;
+        }
+
+        if ("/users".equals(path) && HttpMethod.POST.matches(method)) {
+            return false;
+        }
+
+        if ("/session" .equals(path)) {
+            return false;
+        }
+        return true;
     }
 }
