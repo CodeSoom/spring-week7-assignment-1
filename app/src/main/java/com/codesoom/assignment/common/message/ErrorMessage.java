ee08b82dd1200fad2e3512de51036b6e7aba68d7
Lee Byoung Seong <dev.bslee@gmail.com>
2022-05-07 18:10:36 +0900

    jwt token인증 에러메세지 추가

--- app/src/main/java/com/codesoom/assignment/common/message/ErrorMessage.java
+++ app/src/main/java/com/codesoom/assignment/common/message/ErrorMessage.java
@@ -2,6 +2,12 @@ package com.codesoom.assignment.common.message;
 
 //TODO:다국어 처리 고민
 public enum ErrorMessage {
+    INVALID_USER_BY_TOKEN("인증되지 않은 사용자입니다"),
+    PASSWORD_NOT_MATCH("비밀번호를 확인해주세요"),
+    DELETED_USER("삭제된 유저입니다"),
+    EXPIRED_TOKEN("만료된 토큰입니다"),
+    INVALID_SIGNATURE_TOKEN("서명되지 않은 토큰입니다"),
+    INVALID_TOKEN("토크 검증에 실패했습니다"),
     USER_NOT_EXIST("등록되지 않은 사용자입니다"),
     EMAIL_IS_DUPLICATE("존재하는 이메일 입니다"),
     EMAIL_IS_EMPTY("이메일은 공백이 허용되지 않습니다"),
