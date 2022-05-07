4d70d989d65e865789bcd358b5a62e30ac1cb8f8
Lee Byoung Seong <dev.bslee@gmail.com>
2022-05-07 20:36:06 +0900

    로그인 성공시 메세지 "환영합니다" 메세지 추가

--- app/src/main/java/com/codesoom/assignment/controllers/auth/AuthController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/auth/AuthController.java
@@ -1,6 +1,7 @@
 package com.codesoom.assignment.controllers.auth;
 
 import com.codesoom.assignment.common.CommonResponse;
+import com.codesoom.assignment.common.message.NormalMessage;
 import com.codesoom.assignment.domain.auth.AuthenticationService;
 import com.codesoom.assignment.dto.auth.AuthRequestData;
 import com.codesoom.assignment.dto.auth.AuthResponseData;
@@ -13,14 +14,24 @@ import javax.validation.Valid;
 @RequestMapping("/session")
 @CrossOrigin
 public class AuthController {
-    private AuthenticationService authenticationService;
+    private final AuthenticationService authenticationService;
+
+    public AuthController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    CommonResponse<AuthResponseData> login(@Valid @RequestBody AuthRequestData request) {
+    CommonResponse<AuthResponseData> login(@RequestBody @Valid AuthRequestData request) {
         String accessToken = authenticationService.login(request);
         AuthResponseData authResponseData = new AuthResponseData(accessToken);
-        return CommonResponse.success(authResponseData);
+        StringBuilder loginSuccessBuilder = new StringBuilder();
+        String loginSuccessMessage = loginSuccessBuilder.append("[")
+                                                        .append(request.getEmail()).append("]님")
+                                                        .append(NormalMessage.LOGIN_OK.getNormalMsg())
+                                                        .toString();
+
+        return CommonResponse.success(authResponseData,loginSuccessMessage);
     }
 
 }
