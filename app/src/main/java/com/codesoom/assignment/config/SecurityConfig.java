c7b58fcc7e62233a0bfe07cb46bacfe8c0789c43
jakeseo <n00nietzsche@gmail.com>
2022-05-07 20:34:44 +0900

    인터셉터 설정 제거 -> 필터로 변경

--- app/src/main/java/com/codesoom/assignment/config/SecurityConfig.java
+++ app/src/main/java/com/codesoom/assignment/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.codesoom.assignment.config;
 
+import com.codesoom.assignment.application.AuthenticationService;
+import com.codesoom.assignment.filters.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
@@ -11,12 +14,17 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
+import javax.servlet.Filter;
+
 @Configuration
 // Method Security 참고: https://docs.spring.io/spring-security/reference/servlet/authorization/method-security.html
 // @PreAuthorize, @PostAuthorize, @PreFilter, @PostFilter 애노테이션의 사용을 가능하게 한다.
 // 스프링 시큐리티 최신 버전(5.6 이상)에서는 @EnableMethodSecurity 를 사용한다.
 @EnableGlobalMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final AuthenticationService authenticationService;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -24,9 +32,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        Filter authenticationFilter = new JwtAuthenticationFilter(
+                authenticationManager(), authenticationService
+        );
+
         http.csrf()
                 // 현재는 단순 API 형태로 사용할 것이기 때문에 CSRF 기능을 비활성화한다.
                 .disable()
+                .addFilter(authenticationFilter)
                 .sessionManagement()
                     // 세션, 쿠키방식의 인증 매커니즘을 쓰지 않겠다. 매번 인증을 받겠다.
                     .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
