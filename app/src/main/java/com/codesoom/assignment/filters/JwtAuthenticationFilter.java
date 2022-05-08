41b1735e898ea944177556adba6abf82e65f4337
n00nietzsche <n00nietzsche@gmail.com>
2022-05-08 19:53:39 +0900

    보안 필터가 적용될 Method 와 경로 설정

--- app/src/main/java/com/codesoom/assignment/filters/JwtAuthenticationFilter.java
+++ app/src/main/java/com/codesoom/assignment/filters/JwtAuthenticationFilter.java
@@ -2,6 +2,7 @@ package com.codesoom.assignment.filters;
 
 import com.codesoom.assignment.application.AuthenticationService;
 import com.codesoom.assignment.authentication.UserAuthentication;
+import com.codesoom.assignment.errors.InvalidTokenException;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
@@ -14,36 +15,29 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.HashSet;
 import java.util.Set;
 
-import static org.springframework.util.StringUtils.hasText;
+import static org.springframework.util.StringUtils.isEmpty;
 
 public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
     private final AuthenticationService authenticationService;
-    private final Set<HttpMethod> targetMethods = new HashSet<>();
+    private final Set<String> userPathTargetMethods = Set.of(HttpMethod.PATCH.name(), HttpMethod.DELETE.name(), HttpMethod.PUT.name());
+    private final Set<String> productPathTargetMethods = Set.of(HttpMethod.POST.name(), HttpMethod.PATCH.name(), HttpMethod.DELETE.name(), HttpMethod.PUT.name());
 
     public JwtAuthenticationFilter(AuthenticationManager authenticationManager, AuthenticationService authenticationService) {
         super(authenticationManager);
         this.authenticationService = authenticationService;
-        setTargetMethods();
-    }
-
-    /**
-     * 필터가 적용될 메서드를 설정한다.
-     */
-    private void setTargetMethods() {
-        targetMethods.add(HttpMethod.POST);
-        targetMethods.add(HttpMethod.PATCH);
-        targetMethods.add(HttpMethod.DELETE);
-        targetMethods.add(HttpMethod.PUT);
     }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
-        String authorization = request.getHeader("Authorization");
 
-        if(hasText(authorization)) {
+        String requestURI = request.getRequestURI();
+        String method = request.getMethod();
+        if((requestURI.startsWith("/users") && userPathTargetMethods.contains(method))
+                || (requestURI.startsWith("/products") && productPathTargetMethods.contains(method))
+        ) {
+            String authorization = request.getHeader("Authorization");
             authorize(authorization);
         }
 
@@ -51,6 +45,10 @@ public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
     }
 
     private void authorize(String authorization) {
+        if(isEmpty(authorization)) {
+            throw new InvalidTokenException("토큰이 없습니다.");
+        }
+
         String token = authorization.substring("Bearer ".length());
         Long userId = authenticationService.parseToken(token);
         Authentication authentication = new UserAuthentication(userId);
