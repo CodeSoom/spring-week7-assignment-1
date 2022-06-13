a53f3984d552a42729c75350a88d26265ae03e79
seunglo <devseung9056@gmail.com>
2022-06-14 02:28:35 +0900

    ProductController 사용하지 않는 코드들 정리

--- app/src/main/java/com/codesoom/assignment/controllers/ProductController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/ProductController.java
@@ -20,12 +20,8 @@ import java.util.List;
 public class ProductController {
     private final ProductService productService;
 
-    private final AuthenticationService authenticationService;
-
-    public ProductController(ProductService productService,
-                             AuthenticationService authenticationService) {
+    public ProductController(ProductService productService) {
         this.productService = productService;
-        this.authenticationService = authenticationService;
     }
 
     @GetMapping
@@ -40,28 +36,20 @@ public class ProductController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Product create(
-            @RequestAttribute Long userId,
-            @RequestBody @Valid ProductData productData
-    ) {
+    public Product create(@RequestBody @Valid ProductData productData) {
         return productService.createProduct(productData);
     }
 
     @PatchMapping("{id}")
     public Product update(
-            @RequestAttribute Long userId,
             @PathVariable Long id,
-            @RequestBody @Valid ProductData productData
-    ) {
+            @RequestBody @Valid ProductData productData) {
         return productService.updateProduct(id, productData);
     }
 
     @DeleteMapping("{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void destroy(
-            @RequestAttribute Long userId,
-            @PathVariable Long id
-    ) {
+    public void destroy(@PathVariable Long id) {
         productService.deleteProduct(id);
     }
 }
