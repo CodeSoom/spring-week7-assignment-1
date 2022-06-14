b90313e437471cd81ff977f169a5275a2c84f1a5
seunglo <devseung9056@gmail.com>
2022-06-14 23:45:47 +0900

    UserControllerTest 계층형으로 리팩토링

--- app/src/test/java/com/codesoom/assignment/controllers/UserControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/UserControllerTest.java
@@ -6,10 +6,7 @@ import com.codesoom.assignment.domain.User;
 import com.codesoom.assignment.dto.UserModificationData;
 import com.codesoom.assignment.dto.UserRegistrationData;
 import com.codesoom.assignment.errors.UserNotFoundException;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -21,6 +18,7 @@ import static org.hamcrest.Matchers.containsString;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -43,55 +41,71 @@ class UserControllerTest {
             "eyJ1c2VySWQiOjF9.ZZ3CUl0jxeLGvQ1Js5nG2Ty5qGTlqai5ubDMXZOdaD0";
     private static final Long ACCESS_DENIED_USER_ID = 123L;
 
-    @BeforeEach
-    void setUp() {
-        given(userService.registerUser(any(UserRegistrationData.class)))
-                .will(invocation -> {
-                    UserRegistrationData registrationData = invocation.getArgument(0);
-                    return User.builder()
-                            .id(13L)
-                            .email(registrationData.getEmail())
-                            .name(registrationData.getName())
-                            .build();
-                });
-
-        given(userService.updateUser(eq(100L), any(UserModificationData.class)))
-                .willThrow(new UserNotFoundException(100L));
-
-        given(userService.deleteUser(100L))
-                .willThrow(new UserNotFoundException(100L));
+    @AfterEach
+    void tearDown() {
+        reset(userService);
+        reset(authenticationService);
     }
 
-    @Test
-    void registerUserWithValidAttributes() throws Exception {
-        mockMvc.perform(
-                post("/users")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"email\":\"tester@example.com\"," +
-                                "\"name\":\"Tester\",\"password\":\"test\"}")
-        )
-                .andExpect(status().isCreated())
-                .andExpect(content().string(
-                        containsString("\"id\":13")
-                ))
-                .andExpect(content().string(
-                        containsString("\"email\":\"tester@example.com\"")
-                ))
-                .andExpect(content().string(
-                        containsString("\"name\":\"Tester\"")
-                ));
+    @Nested
+    @DisplayName("POST /users 요청은")
+    class Describe_create {
 
-        verify(userService).registerUser(any(UserRegistrationData.class));
-    }
+        @Nested
+        @DisplayName("유효한 요청 데이터가 주어지면")
+        class Context_with_valid_request_data {
 
-    @Test
-    void registerUserWithInvalidAttributes() throws Exception {
-        mockMvc.perform(
-                post("/users")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content("{}")
-        )
-                .andExpect(status().isBadRequest());
+            @BeforeEach
+            void setUp() {
+                given(userService.registerUser(any(UserRegistrationData.class)))
+                        .will(invocation -> {
+                            UserRegistrationData registrationData = invocation.getArgument(0);
+                            return User.builder()
+                                    .id(13L)
+                                    .email(registrationData.getEmail())
+                                    .name(registrationData.getName())
+                                    .build();
+                        });
+            }
+
+            @Test
+            @DisplayName("생성된 유저를 응답한다.")
+            void it_responses_created_user() throws Exception {
+                mockMvc.perform(
+                                post("/users")
+                                        .contentType(MediaType.APPLICATION_JSON)
+                                        .content("{\"email\":\"tester@example.com\"," +
+                                                "\"name\":\"Tester\",\"password\":\"test\"}")
+                        )
+                        .andExpect(status().isCreated())
+                        .andExpect(content().string(
+                                containsString("\"id\":13")
+                        ))
+                        .andExpect(content().string(
+                                containsString("\"email\":\"tester@example.com\"")
+                        ))
+                        .andExpect(content().string(
+                                containsString("\"name\":\"Tester\"")
+                        ));
+
+                verify(userService).registerUser(any(UserRegistrationData.class));
+            }
+        }
+
+        @Nested
+        @DisplayName("유효하지 않은 요청 데이터가 주어지면")
+        class Context_with_invalid_request_data {
+
+            @Test
+            void it_responses_404_status() throws Exception {
+                mockMvc.perform(
+                                post("/users")
+                                        .contentType(MediaType.APPLICATION_JSON)
+                                        .content("{}")
+                        )
+                        .andExpect(status().isBadRequest());
+            }
+        }
     }
 
     @Nested
@@ -181,44 +195,66 @@ class UserControllerTest {
             }
         }
 
+        @Nested
+        @DisplayName("존재하지 않는 유저 아이디가 주어지면")
+        class Context_with_not_existed_user_id {
+
+            @BeforeEach
+            void setUp() {
+                given(userService.updateUser(eq(100L), any(UserModificationData.class)))
+                        .willThrow(new UserNotFoundException(100L));
+            }
+
+            @Test
+            @DisplayName("404 status를 응답한다.")
+            void it_responses_404_status() throws Exception {
+                mockMvc.perform(
+                                patch("/users/100")
+                                        .contentType(MediaType.APPLICATION_JSON)
+                                        .content("{\"name\":\"TEST\",\"password\":\"TEST\"}")
+                        )
+                        .andExpect(status().isNotFound());
+
+                verify(userService)
+                        .updateUser(eq(100L), any(UserModificationData.class));
+            }
+        }
     }
 
-    @Test
-    void updateUserWithInvalidAttributes() throws Exception {
-        mockMvc.perform(
-                patch("/users/1")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"name\":\"\",\"password\":\"\"}")
-        )
-                .andExpect(status().isBadRequest());
-    }
+    @Nested
+    @DisplayName("DELETE /users 요청은")
+    class Describe_destroy {
 
-    @Test
-    void updateUserWithNotExsitedId() throws Exception {
-        mockMvc.perform(
-                patch("/users/100")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"name\":\"TEST\",\"password\":\"TEST\"}")
-        )
-                .andExpect(status().isNotFound());
+        @Nested
+        @DisplayName("존재하는 유저 아이디가 주어지면")
+        class Context_with_existed_userId {
 
-        verify(userService)
-                .updateUser(eq(100L), any(UserModificationData.class));
-    }
+            @Test
+            @DisplayName("204 status를 응답한다.")
+            void destroyWithExistedId() throws Exception {
+                mockMvc.perform(delete("/users/1"))
+                        .andExpect(status().isNoContent());
 
-    @Test
-    void destroyWithExistedId() throws Exception {
-        mockMvc.perform(delete("/users/1"))
-                .andExpect(status().isNoContent());
+                verify(userService).deleteUser(1L);
+            }
+        }
 
-        verify(userService).deleteUser(1L);
-    }
+        @Nested
+        @DisplayName("존재하지 않는 유저 아이디가 주어지면")
+        class Context_with_not_existed_id {
 
-    @Test
-    void destroyWithNotExistedId() throws Exception {
-        mockMvc.perform(delete("/users/100"))
-                .andExpect(status().isNotFound());
+            @BeforeEach
+            void setUp() {
+                given(userService.deleteUser(100L))
+                        .willThrow(new UserNotFoundException(100L));
+            }
+            @Test
+            void destroyWithNotExistedId() throws Exception {
+                mockMvc.perform(delete("/users/100"))
+                        .andExpect(status().isNotFound());
 
-        verify(userService).deleteUser(100L);
+                verify(userService).deleteUser(100L);
+            }
+        }
     }
 }
