744bd234707e951ae7c579785afbd6142eb30ef6
seunglo <devseung9056@gmail.com>
2022-06-15 19:19:58 +0900

    User의 패스워드를 encode하는 encodePassword 도메인 로직 테스트 추가

--- app/src/test/java/com/codesoom/assignment/domain/UserTest.java
+++ app/src/test/java/com/codesoom/assignment/domain/UserTest.java
@@ -1,10 +1,18 @@
 package com.codesoom.assignment.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class UserTest {
+
+    private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
     @Test
     void changeWith() {
         User user = User.builder().build();
@@ -48,5 +56,40 @@ class UserTest {
 
         assertThat(user.authenticate("test")).isFalse();
         assertThat(user.authenticate("xxx")).isFalse();
+    }
+
+    @Nested
+    @DisplayName("encodePassword 메소드는")
+    class Describe_encodePassword {
+
+        @Nested
+        @DisplayName("패스워드가 널이 아니면")
+        class Context_when_password_is_nonNull {
+            final User user = User.builder()
+                    .password("test")
+                    .build();
+
+            @Test
+            @DisplayName("처음 생성된 패스워드랑 다르다.")
+            void it_is_different_from_first() {
+                user.encodePassword(passwordEncoder);
+
+                assertThat(user.getPassword()).isNotEqualTo("test");
+            }
+        }
+
+        @Nested
+        @DisplayName("패스워드가 널이면")
+        class Context_when_password_is_null {
+            final User user = User.builder()
+                    .build();
+
+            @Test
+            @DisplayName("IllegalStateException 예외를 던진다.")
+            void it_throws_IllegalStateException() {
+                assertThatThrownBy(() -> user.encodePassword(passwordEncoder))
+                        .isInstanceOf(IllegalStateException.class);
+            }
+        }
     }
 }
