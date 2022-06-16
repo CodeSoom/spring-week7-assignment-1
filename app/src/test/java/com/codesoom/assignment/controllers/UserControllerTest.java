03c89016588c247c117e058e4d312e2969aca379
MJbae <bmj5208@gmail.com>
2022-06-16 16:09:37 +0900

    사용자 요청 처리 Controller의 update, destroy 메소드에 대한 테스트 케이스 추가
    작업내용
    - 각각의 메소드에 대해 정상처리 테스트 케이스 추가
    - 각각의 메소드에 대해 요청 데이터 유효성여부 테스트 케이스 추가
    - 각각의 메소드에 대해 인증되지 않은 토큰 입력에 따른 예외 처리 테스트 케이스 추가

--- app/src/test/java/com/codesoom/assignment/controllers/UserControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/UserControllerTest.java
@@ -101,11 +101,83 @@ class UserControllerTest {
                         .andExpect(status().isBadRequest());
             }
         }
+    }
+
+    @Nested
+    @DisplayName("update 메소드는")
+    class Describe_update {
 
         @Nested
-        @DisplayName("인증되지 않은 토큰을 전달받으면")
-        class Context_access_unauthenticated_token {
+        @DisplayName("요청 데이터와 토큰이 모두 유효하다면")
+        class Context_with_valid_request_data_and_token {
 
+            @BeforeEach
+            void setUp() {
+                given(userService.updateUser(eq(USER_ID), any(UserModificationData.class)))
+                        .will(invocation -> {
+                            Long id = invocation.getArgument(0);
+                            UserModificationData modificationData =
+                                    invocation.getArgument(1);
+                            return User.builder()
+                                    .id(id)
+                                    .email("tester@example.com")
+                                    .name(modificationData.getName())
+                                    .build();
+                        });
+
+                given(authenticationService.parseToken(VALID_TOKEN)).willReturn(USER_ID);
+            }
+
+            @Test
+            @DisplayName("HTTP STATUS 200 OK 응답한다")
+            void it_responses_200() throws Exception {
+                mockMvc.perform(
+                                patch("/users/" + USER_ID)
+                                        .contentType(MediaType.APPLICATION_JSON)
+                                        .content("{\"name\":\"TEST\",\"password\":\"test\"}")
+                                        .header(HttpHeaders.AUTHORIZATION, "Bearer " + VALID_TOKEN)
+                        )
+                        .andExpect(status().isOk());
+            }
+        }
+
+        @Nested
+        @DisplayName("요청 데이터가 유효하지 않다면")
+        class Context_with_invalid_request_data {
+            @BeforeEach
+            void setUp() {
+                given(authenticationService.parseToken(VALID_TOKEN)).willReturn(USER_ID);
+
+                given(userService.updateUser(eq(USER_ID), any(UserModificationData.class)))
+                        .will(invocation -> {
+                            Long id = invocation.getArgument(0);
+                            UserModificationData modificationData =
+                                    invocation.getArgument(1);
+                            return User.builder()
+                                    .id(id)
+                                    .email("tester")
+                                    .name(modificationData.getName())
+                                    .build();
+                        });
+
+            }
+
+            @Test
+            @DisplayName("HTTP STATUS 400 BAD REQUEST 응답한다")
+            void it_responses_400() throws Exception {
+                mockMvc.perform(
+                                patch("/users/" + USER_ID)
+                                        .contentType(MediaType.APPLICATION_JSON)
+                                        .content("{}")
+                                        .header(HttpHeaders.AUTHORIZATION, "Bearer " + VALID_TOKEN)
+                        )
+                        .andExpect(status().isBadRequest());
+            }
+        }
+
+        @Nested
+        @DisplayName("인증되지 않은 token 전달 받으면")
+        class Context_with_unauthorized_access_token {
             @BeforeEach
             void setUp() {
                 given(authenticationService.parseToken(UNAUTHORIZED_TOKEN))
@@ -116,13 +188,117 @@ class UserControllerTest {
             @DisplayName("HTTP Status Code 401 UNAUTHORIZED 반환한다")
             void it_responses_401() throws Exception {
                 mockMvc.perform(
-                                post("/users")
+                                patch("/users/" + USER_ID)
                                         .contentType(MediaType.APPLICATION_JSON)
-                                        .content("{\"email\":\"tester@example.com\"," +
-                                                "\"name\":\"Tester\",\"password\":\"test\"}")
+                                        .content("{\"name\":\"TEST\",\"password\":\"test\"}")
                                         .header(HttpHeaders.AUTHORIZATION, "Bearer " + UNAUTHORIZED_TOKEN)
                         )
                         .andExpect(status().isUnauthorized());
+            }
+        }
+
+        @Nested
+        @DisplayName("존재하지 않는 유저 아이디가 주어지면")
+        class Context_with_user_id_not_existing {
+
+            @BeforeEach
+            void setUp() {
+                given(authenticationService.parseToken(VALID_TOKEN))
+                        .willReturn(USER_ID_NOT_EXISTING);
+
+                given(userService.updateUser(eq(USER_ID_NOT_EXISTING), any(UserModificationData.class)))
+                        .willThrow(new UserNotFoundException(USER_ID_NOT_EXISTING));
+            }
+
+            @Test
+            @DisplayName("HTTP STATUS 404 NOT FOUND 응답한다.")
+            void it_responses_404() throws Exception {
+                mockMvc.perform(
+                                patch("/users/" + USER_ID_NOT_EXISTING)
+                                        .contentType(MediaType.APPLICATION_JSON)
+                                        .content("{\"name\":\"TEST\",\"password\":\"TEST\"}")
+                                        .header(HttpHeaders.AUTHORIZATION, "Bearer " + VALID_TOKEN)
+                        )
+                        .andExpect(status().isNotFound());
+
+                verify(userService)
+                        .updateUser(eq(USER_ID_NOT_EXISTING), any(UserModificationData.class));
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("destroy 메소드는")
+    class Describe_destroy {
+
+        @Nested
+        @DisplayName("요청 데이터와 토큰이 모두 유효하다면")
+        class Context_with_valid_request_data_and_token {
+
+            @BeforeEach
+            void setUp() {
+                given(authenticationService.parseToken(VALID_TOKEN))
+                        .willReturn(USER_ID);
+            }
+
+            @Test
+            @DisplayName("HTTP STATUS CODE 204 NO CONTENT 응답한다.")
+            void it_responses_204() throws Exception {
+                mockMvc.perform(
+                                delete("/users/" + USER_ID)
+                                        .header(HttpHeaders.AUTHORIZATION, "Bearer " + VALID_TOKEN)
+                        )
+                        .andExpect(status().isNoContent());
+
+                verify(userService).deleteUser(USER_ID);
+            }
+        }
+
+        @Nested
+        @DisplayName("인증되지 않은 토큰을 전달받으면")
+        class Context_access_unauthorized_token {
+
+            @BeforeEach
+            void setUp() {
+                given(authenticationService.parseToken(UNAUTHORIZED_TOKEN))
+                        .willThrow(new InvalidTokenException(UNAUTHORIZED_TOKEN));
+            }
+
+            @Test
+            @DisplayName("HTTP STATUS CODE 401 UNAUTHORIZED 응답한다")
+            void it_responses_401() throws Exception {
+                mockMvc.perform(
+                                delete("/users/1")
+                                        .header(HttpHeaders.AUTHORIZATION, "Bearer " + UNAUTHORIZED_TOKEN)
+                        )
+                        .andExpect(status().isUnauthorized());
+            }
+        }
+
+
+        @Nested
+        @DisplayName("존재하지 않는 유저 아이디가 주어지면")
+        class Context_with_user_id_not_existing {
+
+            @BeforeEach
+            void setUp() {
+                given(authenticationService.parseToken(VALID_TOKEN))
+                        .willReturn(USER_ID_NOT_EXISTING);
+
+                given(userService.deleteUser(USER_ID_NOT_EXISTING))
+                        .willThrow(new UserNotFoundException(USER_ID_NOT_EXISTING));
+            }
+
+            @Test
+            @DisplayName("HTTP STATUS CODE 404 NOT FOUND 응답한다")
+            void it_responses_404() throws Exception {
+                mockMvc.perform(
+                                delete("/users/" + USER_ID_NOT_EXISTING)
+                                        .header(HttpHeaders.AUTHORIZATION, "Bearer " + VALID_TOKEN)
+                        )
+                        .andExpect(status().isNotFound());
+
+                verify(userService).deleteUser(USER_ID_NOT_EXISTING);
             }
         }
     }
