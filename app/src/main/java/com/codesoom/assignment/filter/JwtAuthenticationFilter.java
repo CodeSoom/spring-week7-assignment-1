99f0cbc4fa5119f3fdc7ad3e276d4d815f8ab34a
minjoon_lee <minjoon1995@naver.com>
2022-06-16 19:58:20 +0900

    fix : filter 에 특정 url 검사 안하도록 설정

--- app/src/main/java/com/codesoom/assignment/filter/JwtAuthenticationFilter.java
+++ app/src/main/java/com/codesoom/assignment/filter/JwtAuthenticationFilter.java
@@ -11,6 +11,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 import com.codesoom.assignment.application.AuthenticationService;
@@ -34,23 +35,46 @@ public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
 
 		String authorization = request.getHeader("Authorization");
 
-		if (authorization == null) {
+		if (filterWithPathAndMethod(request)) {
 			chain.doFilter(request, response);
 			return;
 		}
-
+		if (authorization == null) {
+			throw new InvalidTokenException(null);
+		}
 		String accessToken = authorization.substring("Bearer ".length());
 
 		if (authorization != null) {
 			try {
 				Long userId = authenticationService.parseToken(accessToken);
-				Authentication authentication = new UserAuthentication(userId);
+				UserAuthentication authentication = new UserAuthentication(userId);
+				authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+
 				SecurityContext context = SecurityContextHolder.getContext();
 				context.setAuthentication(authentication);
+
 			} catch (Exception e) {
-				throw new InvalidTokenException("test");
+				throw new InvalidTokenException(authorization);
 			}
 		}
 		chain.doFilter(request, response);
+	}
+	private boolean filterWithPathAndMethod(HttpServletRequest request) {
+		String path = request.getRequestURI();
+		if (!path.startsWith("/products")) {
+			return true;
+		}
+
+		String method = request.getMethod();
+		if (method.equals("GET")) {
+			return true;
+		}
+
+		if (method.equals("OPTIONS")) {
+			return true;
+		}
+
+		return false;
 	}
 }
