581414187a434792dff954001d6e6b42f3db5da7
seunglo <devseung9056@gmail.com>
2022-06-17 02:13:34 +0900

    JwtUtil 하드코딩 된 부분 수정
    매개변수로 userId를 인코딩하지 않고 하드코딩된 아이디 1L을 인코딩 하고 있었음

--- app/src/main/java/com/codesoom/assignment/utils/JwtUtil.java
+++ app/src/main/java/com/codesoom/assignment/utils/JwtUtil.java
@@ -21,7 +21,7 @@ public class JwtUtil {
 
     public String encode(Long userId) {
         return Jwts.builder()
-                .claim("userId", 1L)
+                .claim("userId", userId)
                 .signWith(key)
                 .compact();
     }
