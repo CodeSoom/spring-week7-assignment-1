cfe4a725029508827af9ddcde46c542295449878
HanJiyoung <jiyounghaan@gmail.com>
2022-06-17 06:13:04 +0900

    Builder를 통해 User를 생성할 수 있도록 구현하라

--- app/src/main/java/com/codesoom/assignment/domain/User.java
+++ app/src/main/java/com/codesoom/assignment/domain/User.java
@@ -1,7 +1,30 @@
 package com.codesoom.assignment.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 /**
  * User Entity 클래스
  */
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String email;
+
+    private String name;
+
+    private String password;
 }
