d25e8204ffa305db613ca4a17ec73724ec993917
HanJiyoung <jiyounghaan@gmail.com>
2022-06-16 13:26:59 +0900

    Builder를 통해 Product를 생성할 수 있도록 구현하라

--- app/src/main/java/com/codesoom/assignment/domain/Product.java
+++ app/src/main/java/com/codesoom/assignment/domain/Product.java
@@ -1,7 +1,32 @@
 package com.codesoom.assignment.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 /**
  * Product Entity 클래스
  */
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    private String maker;
+
+    private Integer price;
+
+    private String imageUrl;
 }
