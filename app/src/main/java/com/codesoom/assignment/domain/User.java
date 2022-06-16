f1e5634c2fa8053a4e4d2cb710661480a2368f7e
HanJiyoung <jiyounghaan@gmail.com>
2022-06-17 06:20:55 +0900

    password가 암호화되도록 구현하라

--- app/src/main/java/com/codesoom/assignment/domain/User.java
+++ app/src/main/java/com/codesoom/assignment/domain/User.java
@@ -30,7 +30,14 @@ public class User {
     private String password;
 
     // TODO: 기존의 비밀번호를 암호화해야 한다.
+    /**
+     * 기존의 비밀번호를 매개변수로 주어진 인코더를 통해 암호화한다.
+     *
+     * @param password 암호화 할 비밀번호
+     * @param passwordEncoder 암호화에 사용할 인코더
+     */
     public void encodePassword(String password,
                                PasswordEncoder passwordEncoder) {
+        this.password = passwordEncoder.encode(password);
     }
 }
