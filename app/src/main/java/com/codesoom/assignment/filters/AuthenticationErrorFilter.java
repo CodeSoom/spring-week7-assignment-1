1ff06a586c5d74cf4a45ba0ff0365dd3f36fe98d
yongjunleeme <chesidyong@gmail.com>
2022-06-17 17:16:47 +0900

    AuthenticationErrorFilter 주석을 추가하라

--- app/src/main/java/com/codesoom/assignment/filters/AuthenticationErrorFilter.java
+++ app/src/main/java/com/codesoom/assignment/filters/AuthenticationErrorFilter.java
@@ -10,13 +10,26 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+/**
+ * 인증 에러를 처리한다.
+ */
 public class AuthenticationErrorFilter extends HttpFilter {
 
+    /**
+     * 요청과 응답 쌍을 chain을 통해 전달한다.
+     *
+     * @param request - 요청
+     * @param response - 응답
+     * @param chain - 요청과 응답 쌍을 Chain 내 다음 Entity로 전달
+     * @throws IOException - I/O 명령이 실패한 경우
+     * @throws ServletException - Servlet 에러가 발생한 경우
+     * @throws InvalidTokenException - 토큰이 유효하지 않은 경우
+     */
     @Override
     protected void doFilter(HttpServletRequest request,
                             HttpServletResponse response,
                             FilterChain chain)
-        throws IOException, ServletException {
+        throws IOException, ServletException, InvalidTokenException {
         try {
             chain.doFilter(request, response);
         } catch (InvalidTokenException e) {
