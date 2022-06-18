2778c1109df6eea6e56a87455f5fe1e5e51bf001
MinJoonLee <52944973+mj950425@users.noreply.github.com>
2022-06-18 18:53:12 +0900

    feature : Role 기능이 추가되면서 스프링 컨택스트에 담길 인증 객체에 role list 를 GrantedAuthority list 로 변환해서 담는 로직 추가

--- app/src/main/java/com/codesoom/assignment/security/UserAuthentication.java
+++ app/src/main/java/com/codesoom/assignment/security/UserAuthentication.java
@@ -1,40 +1,45 @@
 package com.codesoom.assignment.security;
 
-import java.util.Arrays;
-import java.util.List;
-
+import com.codesoom.assignment.domain.Role;
+import com.codesoom.assignment.domain.User;
+import lombok.Generated;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
-import lombok.Generated;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class UserAuthentication extends AbstractAuthenticationToken {
 
-	private final Long userId;
+    private final Long userId;
 
-	public UserAuthentication(Long userId) {
-		super(authorities());
-		this.userId = userId;
-	}
+    public UserAuthentication(User user) {
+        super(authorities(user.getRoles()));
+        this.userId = user.getId();
+    }
 
-	@Generated
-	@Override
-	public Object getCredentials() {
-		return null;
-	}
+    @Generated
+    @Override
+    public Object getCredentials() {
+        return null;
+    }
 
-	@Override
-	public Object getPrincipal() {
-		return userId;
-	}
+    @Override
+    public Object getPrincipal() {
+        return userId;
+    }
 
-	@Override
-	public boolean isAuthenticated() {
-		return true;
-	}
+    @Override
+    public boolean isAuthenticated() {
+        return true;
+    }
 
-	private static List<? extends GrantedAuthority> authorities() {
-		return Arrays.asList(new SimpleGrantedAuthority("USER"));
-	}
+    private static List<? extends GrantedAuthority> authorities(List<Role> roles) {
+        List<GrantedAuthority> authorities = roles.stream()
+                .map(role -> new SimpleGrantedAuthority(role.toString()))
+                .collect(Collectors.toList());
+        return Arrays.asList(new SimpleGrantedAuthority("USER"));
+    }
 }
