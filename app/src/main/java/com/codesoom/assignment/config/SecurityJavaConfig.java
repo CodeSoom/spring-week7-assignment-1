8f5a28dd6f6c0215e3cb10a628bc697a6414ef43
yongjunleeme <chesidyong@gmail.com>
2022-06-18 16:23:09 +0900

    configure 코드별 주석을 추가하라

--- app/src/main/java/com/codesoom/assignment/config/SecurityJavaConfig.java
+++ app/src/main/java/com/codesoom/assignment/config/SecurityJavaConfig.java
@@ -23,6 +23,7 @@ public class SecurityJavaConfig extends WebSecurityConfigurerAdapter {
         this.authenticationService = authenticationService;
     }
 
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         Filter authenticationFilter = new JwtAuthenticationFilter(
@@ -30,17 +31,24 @@ public class SecurityJavaConfig extends WebSecurityConfigurerAdapter {
         Filter authenticationErrorFilter = new AuthenticationErrorFilter();
 
         http
-                .headers().frameOptions().disable()
+                .headers().frameOptions().disable() // headers 옵션 중 frameOptions만 비활성화
                 .and()
-                .csrf().disable()
-                .addFilter(authenticationFilter)
+                .csrf().disable()  // csrf 비활성화
+                .addFilter(authenticationFilter) // Form Login에 사용되는 custom AuthenticationFilter 구현체를 등록??
                 .addFilterBefore(authenticationErrorFilter,
-                        JwtAuthenticationFilter.class)
+                        JwtAuthenticationFilter.class) //  JwtAuthenticationFilter
+                // Form Login 시 걸리는 Filter이다. UsernamePasswordAuthenticationFilter를 상속한 JwtAuthenticationFilter을 등록
+                // 이 필터는 HttpServletRequest에서 사용자가 Form으로 입력한 로그인 정보를 인터셉트해서 AuthenticationManager에게
+                // Authentication 객체를 넘겨준다.
                 .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                // 시간 초과 감지, 동시 세션 제어, Session Fixation 공격 보호
+                // SessionManagementFilter, SessionAuthentication 전략
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) //
                 .and()
-                .exceptionHandling()
+                .exceptionHandling() // 예외 핸들러를 등록
                 .authenticationEntryPoint(
+                        // 현재 인증된 사용자가 없는데(SecurityContext에 인증사용자가 담기지 않음)
+                        // 인증되지 않은 사용자가 보호된 리소스에 접근하였을 때, 수행되는 핸들러(EntryPoint)를 등록
                         new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
     }
 }
