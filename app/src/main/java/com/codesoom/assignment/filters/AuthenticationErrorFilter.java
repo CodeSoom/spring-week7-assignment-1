4445724804a753a8491779866012ee8b9f6c5b97
eunice0035 <eunice7357@gmail.com>
2022-08-15 21:48:22 +0900

    interceptor 관련 기능 Security  Filter로 이관2 기능 추가

--- app/src/main/java/com/codesoom/assignment/filters/AuthenticationErrorFilter.java
+++ app/src/main/java/com/codesoom/assignment/filters/AuthenticationErrorFilter.java
@@ -1,2 +1,27 @@
-package com.codesoom.assignment.filters;public class AuthenticationErrorFilter {
+package com.codesoom.assignment.filters;
+
+import com.codesoom.assignment.errors.InvalidTokenException;
+import org.springframework.http.HttpStatus;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AuthenticationErrorFilter extends HttpFilter {
+
+    @Override
+    public void doFilter(HttpServletRequest request,
+                         HttpServletResponse response,
+                         FilterChain chain)
+            throws IOException, ServletException {
+
+        try{
+            chain.doFilter(request, response);
+        } catch(InvalidTokenException e){
+            response.sendError(HttpStatus.UNAUTHORIZED.value());
+        }
+    }
 }
