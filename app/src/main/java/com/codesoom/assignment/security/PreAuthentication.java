0e9db9d577cd4b012f5c71fe48be38a5ba6de5c5
Ernest Hong <hsh3592@gmail.com>
2022-08-21 13:08:56 +0900

    PreAuthentication 주석 추가

--- app/src/main/java/com/codesoom/assignment/security/PreAuthentication.java
+++ app/src/main/java/com/codesoom/assignment/security/PreAuthentication.java
@@ -5,6 +5,10 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+/**
+ * 인증이 되었을 때만 특정 method 가 호출되도록 지정하는 annotation 입니다.
+ * Spring security 의 `@PreAuthorize` 통해서 인증을 확인합니다.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @PreAuthorize("isAuthenticated()")
 public @interface PreAuthentication {
