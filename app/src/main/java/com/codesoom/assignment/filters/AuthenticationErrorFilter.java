0cb6ec069a34a0f44c7f2122f7344a75eb91523a
정현준 <jeongdalma@gmail.com>
2022-09-21 17:33:48 +0900

    try 수정

--- app/src/main/java/com/codesoom/assignment/filters/AuthenticationErrorFilter.java
+++ app/src/main/java/com/codesoom/assignment/filters/AuthenticationErrorFilter.java
@@ -14,10 +14,10 @@ public class AuthenticationErrorFilter extends HttpFilter {
 
     @Override
     protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
-        try{
+        try {
             chain.doFilter(request, response);
         }
-        catch(InvalidTokenException e){
+        catch (InvalidTokenException e){
             response.sendError(HttpStatus.UNAUTHORIZED.value());
         }
     }
