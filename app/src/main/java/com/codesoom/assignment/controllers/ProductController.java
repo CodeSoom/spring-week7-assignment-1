0f8386052fe5438fba6a2ed980c4e09311b86f28
Eunha Jeong <eunhaay@gmail.com>
2022-10-25 20:47:38 +0900

    상품 컨트롤러 시큐리티 `@PreAuthorize` 적용

--- app/src/main/java/com/codesoom/assignment/controllers/ProductController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/ProductController.java
@@ -9,6 +9,7 @@ import com.codesoom.assignment.application.ProductService;
 import com.codesoom.assignment.domain.Product;
 import com.codesoom.assignment.dto.ProductData;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -18,8 +19,8 @@ import java.util.List;
 @RequestMapping("/products")
 @CrossOrigin
 public class ProductController {
-    private final ProductService productService;
 
+    private final ProductService productService;
     private final AuthenticationService authenticationService;
 
     public ProductController(ProductService productService,
@@ -39,29 +40,22 @@ public class ProductController {
     }
 
     @PostMapping
+    @PreAuthorize("isAuthenticated() && hasAuthority('USER')")
     @ResponseStatus(HttpStatus.CREATED)
-    public Product create(
-            @RequestAttribute Long userId,
-            @RequestBody @Valid ProductData productData
-    ) {
+    public Product create(@RequestBody @Valid ProductData productData) {
         return productService.createProduct(productData);
     }
 
     @PatchMapping("{id}")
-    public Product update(
-            @RequestAttribute Long userId,
-            @PathVariable Long id,
-            @RequestBody @Valid ProductData productData
-    ) {
+    @PreAuthorize("isAuthenticated() && hasAuthority('USER')")
+    public Product update(@PathVariable Long id, @RequestBody @Valid ProductData productData) {
         return productService.updateProduct(id, productData);
     }
 
     @DeleteMapping("{id}")
+    @PreAuthorize("isAuthenticated() && hasAuthority('USER')")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void destroy(
-            @RequestAttribute Long userId,
-            @PathVariable Long id
-    ) {
+    public void destroy(@PathVariable Long id) {
         productService.deleteProduct(id);
     }
 }
