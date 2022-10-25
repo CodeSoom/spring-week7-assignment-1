16c7fb64af56029a0f00687034b5526ef9072e2e
Eunha Jeong <eunhaay@gmail.com>
2022-10-25 20:28:29 +0900

    인증 서비스 코드 수정
    - 주석을 추가했습니다.
    - login 메소드에서 map을 사용하도록 변경했습니다.
    - parseToken에서 발생할 예외를 체크하는 코드를 추가했습니다.

--- app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
+++ app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
@@ -1,10 +1,11 @@
 package com.codesoom.assignment.application;
 
-import com.codesoom.assignment.domain.User;
 import com.codesoom.assignment.domain.UserRepository;
+import com.codesoom.assignment.errors.InvalidTokenException;
 import com.codesoom.assignment.errors.LoginFailException;
 import com.codesoom.assignment.utils.JwtUtil;
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.RequiredTypeException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,19 +19,40 @@ public class AuthenticationService {
         this.jwtUtil = jwtUtil;
     }
 
+    /**
+     * 회원 로그인을 처리하고 토큰을 반환합니다.
+     * @param email 로그인할 이메일
+     * @param password 비밀번호
+     * @return 토큰
+     * @throws LoginFailException 회원을 찾지 못하거나 비밀번호가 틀린 경우
+     */
     public String login(String email, String password) {
-        User user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new LoginFailException(email));
+        return userRepository.findByEmail(email)
+                .map(user -> {
+                    if (!user.authenticate(password)) {
+                        throw new LoginFailException(email);
+                    }
 
-        if (!user.authenticate(password)) {
-            throw new LoginFailException(email);
-        }
-
-        return jwtUtil.encode(1L);
+                    return jwtUtil.encode(user.getId());
+                }).orElseThrow(() -> new LoginFailException(email));
     }
 
+    /**
+     * 토큰에서 회원 정보를 꺼냅니다.
+     * @param accessToken 토큰
+     * @return 회원 정보
+     * @throws InvalidTokenException 토큰의 클레임에서 회원 정보를 꺼낼 수 없는 경우
+     */
     public Long parseToken(String accessToken) {
         Claims claims = jwtUtil.decode(accessToken);
-        return claims.get("userId", Long.class);
+        try {
+            Long userId = claims.get("userId", Long.class);
+            if (userId == null) {
+                throw new InvalidTokenException("토큰의 클레임 값이 비어있습니다.");
+            }
+            return userId;
+        } catch (RequiredTypeException e) {
+            throw new InvalidTokenException("토큰의 클레임 값이 잘못되었습니다.");
+        }
     }
 }
