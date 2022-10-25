04f2317b663fc0e885a5cd5c38b59d71006bd37f
Eunha Jeong <eunhaay@gmail.com>
2022-10-25 20:25:00 +0900

    토큰 유효성 검사 메소드 추가
    - 회원 인증을 수행할 필터에서 헤더값으로 들어온 문자열을 토큰으로 파싱할 수 있는지 확인할 수 있도록 유효성 검사 메소드와 테스트를 추가했습니다.
    - InvalidTokenException 생성시 토큰이 아닌 메시지를 받도록 변경했습니다.

--- app/src/main/java/com/codesoom/assignment/utils/JwtUtil.java
+++ app/src/main/java/com/codesoom/assignment/utils/JwtUtil.java
@@ -2,14 +2,21 @@ package com.codesoom.assignment.utils;
 
 import com.codesoom.assignment.errors.InvalidTokenException;
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SecurityException;
 import io.jsonwebtoken.security.SignatureException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import java.security.Key;
 
+@Slf4j
 @Component
 public class JwtUtil {
     private final Key key;
@@ -18,16 +25,26 @@ public class JwtUtil {
         key = Keys.hmacShaKeyFor(secret.getBytes());
     }
 
+    /**
+     * 토큰을 생성합니다.
+     * @param userId 토큰에 담을 회원 정보
+     * @return 생성한 토큰
+     */
     public String encode(Long userId) {
         return Jwts.builder()
-                .claim("userId", 1L)
+                .claim("userId", userId)
                 .signWith(key)
                 .compact();
     }
 
+    /**
+     * 토큰을 해석합니다.
+     * @param token 토큰
+     * @return 토큰 정보
+     */
     public Claims decode(String token) {
         if (token == null || token.isBlank()) {
-            throw new InvalidTokenException(token);
+            throw new InvalidTokenException("토큰이 비어있습니다.");
         }
 
         try {
@@ -37,7 +54,34 @@ public class JwtUtil {
                     .parseClaimsJws(token)
                     .getBody();
         } catch (SignatureException e) {
-            throw new InvalidTokenException(token);
+            throw new InvalidTokenException("토큰 해석에 실패했습니다. :" + e.getMessage());
+        }
+    }
+
+    /**
+     * 토큰의 유효성을 검사하여 유효한 토큰이면 true를 반환합니다.
+     * @param token 토큰
+     * @return 유효한 토큰이면 true, 아니면 false
+     */
+    public boolean validateToken(String token) {
+        if (!StringUtils.hasText(token)) {
+            return false;
+        }
+
+        try {
+            Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token);
+
+            return true;
+        } catch (SecurityException
+                 | MalformedJwtException
+                 | ExpiredJwtException
+                 | UnsupportedJwtException
+                 | IllegalArgumentException e) {
+            log.error(e.getMessage());
+            return false;
         }
     }
 }
--- app/src/test/java/com/codesoom/assignment/utils/JwtUtilTest.java
+++ app/src/test/java/com/codesoom/assignment/utils/JwtUtilTest.java
@@ -3,7 +3,12 @@ package com.codesoom.assignment.utils;
 import com.codesoom.assignment.errors.InvalidTokenException;
 import io.jsonwebtoken.Claims;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -53,5 +58,57 @@ class JwtUtilTest {
 
         assertThatThrownBy(() -> jwtUtil.decode("   "))
                 .isInstanceOf(InvalidTokenException.class);
+    }
+
+    @Nested
+    @DisplayName("validateToken 메소드는")
+    class Describe_validateToken {
+
+        @Nested
+        @DisplayName("유효한 토큰이 주어지면")
+        class Context_with_valid_token {
+            private String token;
+
+            @BeforeEach
+            void setUp() {
+                token = jwtUtil.encode(1L);
+            }
+
+            @Test
+            @DisplayName("true를 반환한다")
+            void it_returns_true() {
+                assertThat(jwtUtil.validateToken(token)).isTrue();
+            }
+        }
+
+        @Nested
+        @DisplayName("null이나 공백이 주어지면")
+        class Context_with_null_or_white_space {
+
+            @ParameterizedTest
+            @NullAndEmptySource
+            @ValueSource(strings = {"\n", "\t", " "})
+            @DisplayName("false를 반환한다")
+            void it_throws_exception(String token) {
+                assertThat(jwtUtil.validateToken(token)).isFalse();
+            }
+        }
+
+        @Nested
+        @DisplayName("토큰으로 해석할 수 없는 문자열이 들어오면")
+        class Context_with_wrong_token {
+
+            @ParameterizedTest
+            @ValueSource(strings = {
+                    "a", "a.a", "a.a.a", "bbb.b.bb", "23.232.33",
+                    "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjQ0fQ...eAG4-BS7VDgku9pNxKwyQHqom8EOd3qmBeDOudK2g0s",
+                    "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjQ0fQ.eAG4-BS7VDgku9pNxKwyQHqom8EOd3qmBeDOudK2g0s11111",
+                    "eyJhbGciOJ9.eyJ1ciOjQ0fQ.eAG4OudK2g0s"
+            })
+            @DisplayName("false를 반환한다")
+            void it_throws_exception(String token) {
+                assertThat(jwtUtil.validateToken(token)).isFalse();
+            }
+        }
     }
 }
