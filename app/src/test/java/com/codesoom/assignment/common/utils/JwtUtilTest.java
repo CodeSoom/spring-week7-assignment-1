d4ebc8926c974746d6cdbac7db75d5f74480e805
giibeom <kpmyung@gmail.com>
2022-11-23 01:15:52 +0900

    refactor: JwtUtil 유닛 테스트 BDD 스타일로 변경 및 리팩토링

--- app/src/test/java/com/codesoom/assignment/common/utils/JwtUtilTest.java
+++ app/src/test/java/com/codesoom/assignment/common/utils/JwtUtilTest.java
@@ -1,57 +1,81 @@
 package com.codesoom.assignment.common.utils;
 
 import com.codesoom.assignment.session.application.exception.InvalidTokenException;
-import io.jsonwebtoken.Claims;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import static com.codesoom.assignment.support.AuthHeaderFixture.INVALID_VALUE_TOKEN_1;
+import static com.codesoom.assignment.support.AuthHeaderFixture.VALID_TOKEN_1;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class JwtUtilTest {
-    private static final String SECRET = "12345678901234567890123456789012";
+    private static final String SECRET = VALID_TOKEN_1.시크릿_키();
 
-    private static final String VALID_TOKEN = "eyJhbGciOiJIUzI1NiJ9." +
-            "eyJ1c2VySWQiOjF9.ZZ3CUl0jxeLGvQ1Js5nG2Ty5qGTlqai5ubDMXZOdaDk";
-    private static final String INVALID_TOKEN = "eyJhbGciOiJIUzI1NiJ9." +
-            "eyJ1c2VySWQiOjF9.ZZ3CUl0jxeLGvQ1Js5nG2Ty5qGTlqai5ubDMXZOdaD0";
+    private final JwtUtil jwtUtil = new JwtUtil(SECRET);
 
-    private JwtUtil jwtUtil;
+    @Nested
+    @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+    class encode_메서드는 {
 
-    @BeforeEach
-    void setUp() {
-        jwtUtil = new JwtUtil(SECRET);
+        @Test
+        @DisplayName("토큰을 secretKey로 인코딩하여 리턴한다")
+        void it_returns_token() {
+            String token = jwtUtil.encode(VALID_TOKEN_1.아이디());
+
+            assertThat(token).isEqualTo(VALID_TOKEN_1.토큰_값());
+        }
     }
 
-    @Test
-    void encode() {
-        String token = jwtUtil.encode(1L);
+    @Nested
+    @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+    class decode_메서드는 {
 
-        assertThat(token).isEqualTo(VALID_TOKEN);
-    }
+        @Nested
+        @DisplayName("유효한 토큰이 주어질 경우")
+        class Context_with_valid_token {
 
-    @Test
-    void decodeWithValidToken() {
-        Claims claims = jwtUtil.decode(VALID_TOKEN);
+            @Test
+            @DisplayName("userId가 포함된 클레임을 리턴한다")
+            void it_returns_claims() {
+                Long userId = jwtUtil.decode(VALID_TOKEN_1.토큰_값())
+                        .get("userId", Long.class);
 
-        assertThat(claims.get("userId", Long.class)).isEqualTo(1L);
-    }
+                assertThat(userId).isEqualTo(VALID_TOKEN_1.아이디());
+            }
+        }
 
-    @Test
-    void decodeWithInvalidToken() {
-        assertThatThrownBy(() -> jwtUtil.decode(INVALID_TOKEN))
-                .isInstanceOf(InvalidTokenException.class);
-    }
+        @Nested
+        @DisplayName("유효하지 않은 토큰이 주어질 경우")
+        class Context_with_invalid_token {
 
-    @Test
-    void decodeWithEmptyToken() {
-        assertThatThrownBy(() -> jwtUtil.decode(null))
-                .isInstanceOf(InvalidTokenException.class);
+            @Test
+            @DisplayName("InvalidTokenException 예외를 던진다")
+            void it_returns_exception() {
+                assertThatThrownBy(() -> jwtUtil.decode(INVALID_VALUE_TOKEN_1.토큰_값()))
+                        .isInstanceOf(InvalidTokenException.class);
+            }
+        }
 
-        assertThatThrownBy(() -> jwtUtil.decode(""))
-                .isInstanceOf(InvalidTokenException.class);
+        @Nested
+        @DisplayName("비어있는 토큰이 주어질 경우")
+        class Context_with_blank_token {
 
-        assertThatThrownBy(() -> jwtUtil.decode("   "))
-                .isInstanceOf(InvalidTokenException.class);
+            @Test
+            @DisplayName("InvalidTokenException 예외를 던진다")
+            void it_returns_exception() {
+                assertThatThrownBy(() -> jwtUtil.decode(null))
+                        .isInstanceOf(InvalidTokenException.class);
+
+                assertThatThrownBy(() -> jwtUtil.decode(""))
+                        .isInstanceOf(InvalidTokenException.class);
+
+                assertThatThrownBy(() -> jwtUtil.decode("   "))
+                        .isInstanceOf(InvalidTokenException.class);
+            }
+        }
     }
 }
