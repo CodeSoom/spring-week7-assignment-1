f535f7af54e740a7d8f4d79902975ade7be03b29
giibeom <kpmyung@gmail.com>
2022-11-26 02:57:10 +0900

    refactor(controllerTest): 기존 Controller MockMvc 테스트에서 반환 값 검증 제거
    - 기존에는 각 필드별로 세세히 검증했지만, Controller 테스트의 목적은 반환 값 검증이 메인이 아님
    - 응답 상태 코드 검증 및 내부 메서드 호출 검증에 집중

--- app/src/test/java/com/codesoom/assignment/user/presentation/UserControllerMockTest.java
+++ app/src/test/java/com/codesoom/assignment/user/presentation/UserControllerMockTest.java
@@ -33,7 +33,6 @@ import static com.codesoom.assignment.support.UserFixture.USER_1;
 import static com.codesoom.assignment.support.UserFixture.USER_INVALID_EMAIL;
 import static com.codesoom.assignment.support.UserFixture.USER_INVALID_NAME;
 import static com.codesoom.assignment.support.UserFixture.USER_INVALID_PASSWORD;
-import static org.hamcrest.Matchers.containsString;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
@@ -42,7 +41,6 @@ import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest({UserController.class, MockMvcCharacterEncodingCustomizer.class})
@@ -98,7 +96,6 @@ class UserControllerMockTest {
                 ResultActions perform = 회원_등록_API_요청(USER_1);
 
                 perform.andExpect(status().isCreated());
-                USER_이름_이메일_값_검증(perform, USER_1);
 
                 verify(userService).registerUser(any(UserRegistrationData.class));
             }
@@ -174,9 +171,6 @@ class UserControllerMockTest {
 
                 perform.andExpect(status().isUnauthorized());
 
-                // 아니 왜 해당 테스트를 단일로 실행하면 통과 되는데, 전체 실행을 하면 invoke가 됐다고 실패가 되지...?
-                // 해당 구문은 isAuthenticated()에서 실패돼서 컨트롤러로 못들어오는 로직이여야 하는데..
-                // 심지어 위에 status().isUnauthorized() 코드는 검증 성공으로 뜸.... (verfiy 구문 주석하면 테스트 올패스)
                 verify(userService, never()).updateUser(eq(ID_MIN.value()), any(UserModificationData.class));
             }
         }
@@ -196,9 +190,6 @@ class UserControllerMockTest {
 
                 perform.andExpect(status().isUnauthorized());
 
-                // 아니 왜 해당 테스트를 단일로 실행하면 통과 되는데, 전체 실행을 하면 invoke가 됐다고 실패가 되지...?
-                // 해당 구문은 isAuthenticated()에서 실패돼서 컨트롤러로 못들어오는 로직이여야 하는데..
-                // 심지어 위에 status().isUnauthorized() 코드는 검증 성공으로 뜸.... (verfiy 구문 주석하면 테스트 올패스)
                 verify(userService, never()).updateUser(eq(ID_MIN.value()), any(UserModificationData.class));
             }
         }
@@ -231,7 +222,6 @@ class UserControllerMockTest {
                         );
 
                         perform.andExpect(status().isOk());
-                        USER_이름_이메일_값_검증(perform, USER_1);
 
                         verify(userService).updateUser(eq(ID_MIN.value()), any(UserModificationData.class));
                     }
@@ -325,9 +315,6 @@ class UserControllerMockTest {
 
                 perform.andExpect(status().isUnauthorized());
 
-                // 아니 왜 해당 테스트를 단일로 실행하면 통과 되는데, 전체 실행을 하면 invoke가 됐다고 실패가 되지...?
-                // 해당 구문은 isAuthenticated()에서 실패돼서 컨트롤러로 못들어오는 로직이여야 하는데..
-                // 심지어 위에 status().isUnauthorized() 코드는 검증 성공으로 뜸.... (verfiy 구문 주석하면 테스트 올패스)
                 verify(userService, never()).deleteUser(ID_MIN.value());
             }
         }
@@ -346,9 +333,6 @@ class UserControllerMockTest {
 
                 perform.andExpect(status().isUnauthorized());
 
-                // 아니 왜 해당 테스트를 단일로 실행하면 통과 되는데, 전체 실행을 하면 invoke가 됐다고 실패가 되지...?
-                // 해당 구문은 isAuthenticated()에서 실패돼서 컨트롤러로 못들어오는 로직이여야 하는데..
-                // 심지어 위에 status().isUnauthorized() 코드는 검증 성공으로 뜸.... (verfiy 구문 주석하면 테스트 올패스)
                 verify(userService, never()).deleteUser(ID_MIN.value());
             }
         }
@@ -426,10 +410,5 @@ class UserControllerMockTest {
                 delete(REQUEST_USER_URL + "/" + userId)
                         .header(HttpHeaders.AUTHORIZATION, authHeader)
         );
-    }
-
-    private void USER_이름_이메일_값_검증(ResultActions perform, UserFixture userFixture) throws Exception {
-        perform.andExpect(content().string(containsString(userFixture.이름())));
-        perform.andExpect(content().string(containsString(userFixture.이메일())));
     }
 }
