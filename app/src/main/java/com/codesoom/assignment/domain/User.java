66185955a68723d2af6dd44f8fba5f89507d0878
EuijeongHwang_Mac <euijeong.hwang97@gmail.com>
2022-12-21 20:45:41 +0900

    Feat: User 도메인에 다대다 조인 추가
    user:user_authority 와 일대다, user_authority:authority와 다대일 -> 다대다 조인
    회원가입 시 user_authority 테이블에 user의 아이디와 authority 이름이 조인되어 데이터 추가됨

--- app/src/main/java/com/codesoom/assignment/domain/User.java
+++ app/src/main/java/com/codesoom/assignment/domain/User.java
@@ -6,9 +6,18 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -17,7 +26,8 @@ import javax.persistence.Id;
 @AllArgsConstructor
 public class User {
     @Id
-    @GeneratedValue
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String email;
@@ -28,6 +38,14 @@ public class User {
 
     @Builder.Default
     private boolean deleted = false;
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_authority",
+            joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "user_id")},
+            inverseJoinColumns = {@JoinColumn(name = "authority_name", referencedColumnName = "authority_name")}
+    )
+    private Set<Authority> authorities;
 
     public void changeWith(User source) {
         name = source.name;
