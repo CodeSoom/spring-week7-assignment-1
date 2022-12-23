fa1eaf01e878a30e838f901a0e3921b18b219668
EuijeongHwang_Mac <euijeong.hwang97@gmail.com>
2022-12-23 17:15:07 +0900

    Feat: 해당 id의 권한 이름을 찾는 쿼리 추가
    해당 id의 권한을 찾는 메소드 추가함
    직접 쿼리를 작성하는 것이 더 간편하여 직접 쿼리로 작성함

--- app/src/main/java/com/codesoom/assignment/repository/UserRepository.java
+++ app/src/main/java/com/codesoom/assignment/repository/UserRepository.java
@@ -1,13 +1,20 @@
 package com.codesoom.assignment.repository;
 
+import com.codesoom.assignment.domain.Authority;
 import com.codesoom.assignment.domain.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
+
+    List<User> findAll();
+
     User save(User user);
 
     boolean existsByEmail(String email);
@@ -17,4 +24,10 @@ public interface UserRepository extends CrudRepository<User, Long> {
     Optional<User> findByIdAndDeletedIsFalse(Long id);
 
     Optional<User> findByEmail(String email);
+
+    @Query(
+            value = "select authority_name from user_authority where user_id = ?",
+            nativeQuery = true
+    )
+    Set<Authority> findAuthorityNameById(long id);
 }
