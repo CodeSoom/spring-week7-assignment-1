7e94f4ba296b180fa5c1ae37cdd32d36fe5a4d54
tmxhsk99 <tmxhsk99@naver.com>
2023-08-18 06:11:05 +0900

    feat : 유저 패스워드 암호화 기능 추가
    평문의 패스워드를 암호화하도록 기능 추가

--- app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
+++ app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
@@ -4,24 +4,26 @@ import com.codesoom.assignment.domain.User;
 import com.codesoom.assignment.domain.UserRepository;
 import com.codesoom.assignment.errors.LoginFailException;
 import com.codesoom.assignment.utils.JwtUtil;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthenticationService {
     private final UserRepository userRepository;
     private final JwtUtil jwtUtil;
+    private PasswordEncoder passwordEncoder;
 
-    public AuthenticationService(UserRepository userRepository,
-                                 JwtUtil jwtUtil) {
+    public AuthenticationService(UserRepository userRepository, JwtUtil jwtUtil, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.jwtUtil = jwtUtil;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public String login(String email, String password) {
         User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new LoginFailException(email));
 
-        if (!user.authenticate(password)) {
+        if (!user.authenticate(password, passwordEncoder)) {
             throw new LoginFailException(email);
         }
 
--- app/src/main/java/com/codesoom/assignment/config/SecurityJavaConfig.java
+++ app/src/main/java/com/codesoom/assignment/config/SecurityJavaConfig.java
@@ -3,12 +3,15 @@ package com.codesoom.assignment.config;
 import com.codesoom.assignment.filters.AuthenticationErrorFilter;
 import com.codesoom.assignment.filters.JwtAuthenticationFilter;
 import com.codesoom.assignment.utils.JwtUtil;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 import javax.servlet.Filter;
@@ -83,4 +86,6 @@ public class SecurityJavaConfig extends WebSecurityConfigurerAdapter {
     private boolean matchesDeleteUserRequest(HttpServletRequest req) {
         return req.getMethod().equals("DELETE") && req.getRequestURI().matches("^/users/[0-9]+$");
     }
+
+
 }
